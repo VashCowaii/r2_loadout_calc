38ce6548d06bbd43888cac9cc534a4096a879132
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2024-03-24 14:05:04 -0600

    yml test v2.1

--- buildTime.js
+++ buildTime.js
@@ -1,5 +1,7 @@
-let currentTime = new Date();
-let utcTime = currentTime.getTime();
-let mstOffset = currentTime.getTimezoneOffset() * 60 * 1000 - 6 * 60 * 60 * 1000;
-let mountainTime = new Date(utcTime + mstOffset);
-const lastCalcUpdate = mountainTime.toISOString().split('T')[0] + ' ' + mountainTime.getHours() + ':00MST';
+// let currentTime = new Date();
+// let utcTime = currentTime.getTime();
+// let mstOffset = currentTime.getTimezoneOffset() * 60 * 1000 - 6 * 60 * 60 * 1000;
+// let mountainTime = new Date(utcTime + mstOffset);
+// const lastCalcUpdate = mountainTime.toISOString().split('T')[0] + ' ' + mountainTime.getHours() + ':00MST';
+
+const lastCalcUpdate = 'Day, Month Date, Hour:Minute:MST';
