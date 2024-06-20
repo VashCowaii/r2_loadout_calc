a3c0bf3bc3aa46497486392b2cafbbe14378a837
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2024-06-19 22:36:00 -0600

    More tooltip fixes + cleaning

--- Remnant2/Calculator/tooltips.js
+++ Remnant2/Calculator/tooltips.js
@@ -7,150 +7,37 @@ const tooltipStorage = {
 
 };
 
-// function addTooltipListeners() {
-//     document.querySelectorAll('.hasHoverTooltip').forEach(element => {
-//         // Check for custom data attributes that indicate the presence of listeners
-//         const hasMouseEnterListener = element.getAttribute('data-mouseenter-listener') === 'true';
-//         const hasMouseLeaveListener = element.getAttribute('data-mouseleave-listener') === 'true';
-    
-//         if (!hasMouseEnterListener) {
-//             element.addEventListener('mouseenter', () => showTooltip(element.id));
-//             element.setAttribute('data-mouseenter-listener', 'true'); // Mark that mouseenter listener has been added
-//         }
-    
-//         if (!hasMouseLeaveListener) {
-//             element.addEventListener('mouseleave', hideTooltip);
-//             element.setAttribute('data-mouseleave-listener', 'true'); // Mark that mouseleave listener has been added
-//         }
-//     });
-// }
-// addTooltipListeners();
-
-function checkItemForStat(path,name,statistic,type) {
-    let tags = path.tags
-
-    let newTagArray = [];
-
-    if (!type) {
-        let stats = Object.keys(path.stats);
-        for (entry of stats) {
-            newTagArray.push(entry);
-        }
-    }
-    if (type != "prime") {
-        let tags = path.tags
-        for (entry of tags) {
-            newTagArray.push(entry);
-        }
-    }
-    else {
-        let stats = Object.keys(path.primeStats);
-        for (entry of stats) {
-            newTagArray.push(entry);
-        }
-    }
-    // }
-    // else {
-    //     if (path.property)
-    // }
-
-    let itemFound = false;
-
-    for (entry of newTagArray) {
-        if (entry === statistic) {itemFound = true;}
-    }
-
-    return itemFound ? `${name}<br>` : "";
-}
-
-function returnItemsWithStat(statistic) {
-    let htmlString = "";
-    let globalReference = globalRecords;
-    let accessoryReference = globalReference.accessories;
-    let consumableReference = globalReference.consumables;
-    let weaponReference = globalReference.weapons;
-    let armorReference = globalReference.armor;
-    let traitReference = globalReference.greatTraitRecords
-    let classReference = globalReference.archs;
-
-    htmlString += checkItemForStat(amulets[accessoryReference.amulet],accessoryReference.amulet,statistic);
-    for (let i=1;i<=4;i++) {
-        htmlString += checkItemForStat(rings[accessoryReference[`ring${i}`]],accessoryReference[`ring${i}`],statistic);
-    }
-    htmlString += checkItemForStat(relics[accessoryReference.relic],accessoryReference.relic,statistic);
-    for (let i=1;i<=3;i++) {
-        htmlString += checkItemForStat(fragments[accessoryReference[`fragment${i}`]],accessoryReference[`fragment${i}`],statistic);
-    }
-
-    for (let i=1;i<=7;i++) {
-        htmlString += checkItemForStat(concoctions[consumableReference[`concoction${i}`]],consumableReference[`concoction${i}`],statistic);
-    }
-    for (let i=1;i<=4;i++) {
-        htmlString += checkItemForStat(quickUses[consumableReference[`quickUse${i}`]],consumableReference[`quickUse${i}`],statistic);
-    }
-    htmlString += checkItemForStat(primary[weaponReference.primary],weaponReference.primary,statistic);
-    htmlString += checkItemForStat(rangedMutators[weaponReference.primaryMutator],weaponReference.primaryMutator,statistic);
-    htmlString += primary[weaponReference.primary].builtIN ? checkItemForStat(builtInPrimary[primary[weaponReference.primary].builtIN],primary[weaponReference.primary].builtIN,statistic)
-    : checkItemForStat(rangedMods[weaponReference.primaryMod],weaponReference.primaryMod,statistic);
-    htmlString += checkItemForStat(melee[weaponReference.melee],weaponReference.melee,statistic);
-    htmlString += checkItemForStat(meleeMutators[weaponReference.meleeMutator],weaponReference.meleeMutator,statistic);
-    htmlString += melee[weaponReference.melee].builtIN ? checkItemForStat(builtInMelee[melee[weaponReference.melee].builtIN],melee[weaponReference.melee].builtIN,statistic)
-    : "";
-    htmlString += checkItemForStat(secondary[weaponReference.secondary],weaponReference.secondary,statistic);
-    htmlString += checkItemForStat(rangedMutators[weaponReference.secondaryMutator],weaponReference.secondaryMutator,statistic);
-    htmlString += secondary[weaponReference.secondary].builtIN ? checkItemForStat(builtInSecondary[secondary[weaponReference.secondary].builtIN],secondary[weaponReference.secondary].builtIN,statistic)
-    : checkItemForStat(rangedMods[weaponReference.secondaryMod],weaponReference.secondaryMod,statistic);
-    htmlString += checkItemForStat(helmets[armorReference.helmet],armorReference.helmet,statistic);
-    htmlString += checkItemForStat(chests[armorReference.chest],armorReference.chest,statistic);
-    htmlString += checkItemForStat(legs[armorReference.leg],armorReference.leg,statistic);
-    htmlString += checkItemForStat(hands[armorReference.hand],armorReference.hand,statistic);
-
-    for (let i=0;i<traitReference.length;i++) {
-        htmlString += checkItemForStat(traits[traitReference[i].name],traitReference[i].name,statistic,"trait");
-    }
-
-
-    htmlString += checkItemForStat(classInfo[classReference.one.class],classInfo[classReference.one.class].primePerk,statistic,"prime");
-    htmlString += checkItemForStat(classInfo[classReference.one.class].abilities[classReference.one.ability],classReference.one.ability,statistic);
-    for (let i=1;i<=4;i++) {
-        htmlString += checkItemForStat(classInfo[classReference.one.class].passives[`passive${i}`],classInfo[classReference.one.class].passives[`passive${i}`].name,statistic);
-    }
-
-    htmlString += checkItemForStat(classInfo[classReference.two.class].abilities[classReference.two.ability],classReference.two.ability,statistic);
-    for (let i=1;i<=4;i++) {
-        htmlString += checkItemForStat(classInfo[classReference.two.class].passives[`passive${i}`],classInfo[classReference.two.class].passives[`passive${i}`].name,statistic);
-    }
-
-    // classReference
-
-
-    return htmlString;
-}
-
 let tooltips = {
     hideTooltip() {
         const tooltip = document.getElementById('tooltip');
         tooltip.style.display = 'none';
     },
     showTooltip(elementId) {
-        // console.log("triggered")
         const tooltip = document.getElementById('tooltip');
         const element = document.getElementById(elementId);
         const tooltipText = tooltipStorage[elementId];
-        const isMobile = window.matchMedia("(max-width: 768px)").matches; // Adjust the breakpoint as needed
+        const isMobile = window.matchMedia("(max-width: 768px)").matches;//Adjust the breakpoint as needed
     
         if (tooltipText) {
             tooltip.innerHTML = userTrigger.updateSubstatColor(tooltipText);
             tooltip.style.display = 'block';
     
             if (isMobile) {
-                // On mobile view, fit to screen width and position beneath the tapped element
-                tooltip.style.width = 'calc(100% - 20px)'; // Adjust as needed
-                tooltip.style.left = '10px'; // Center it horizontally with some margin
+                //On mobile view, fit to screen width and position beneath the tapped element
+                tooltip.style.width = 'calc(100% - 20px)';//Adjust as needed
+                tooltip.style.left = '10px';//Center it horizontally with some margin
                 const rect = element.getBoundingClientRect();
-                tooltip.style.top = window.scrollY + rect.bottom + 10 + 'px'; // Position it below the element with some margin
+                tooltip.style.top = window.scrollY + rect.bottom + 10 + 'px';//Position it below the element with some margin
+    
+                //Add event listener to close tooltip if tapping outside
+                document.addEventListener('click', function outsideClickListener(event) {
+                    if (!tooltip.contains(event.target) && !element.contains(event.target)) {
+                        tooltips.hideTooltip();
+                        document.removeEventListener('click', outsideClickListener);
+                    }
+                });
             } else {
-                // On desktop view, position according to the mouse cursor
+                //On desktop view, position according to the mouse cursor
                 element.addEventListener('mousemove', function (event) {
                     tooltip.style.left = event.pageX + 10 + 'px';
                     tooltip.style.top = event.pageY + 10 + 'px';
@@ -203,12 +90,102 @@ let tooltips = {
             }
         });
     },
+    checkItemForStat(path,name,statistic,type) {
+        //Checks the path provided for a given statistic, and if the stat exists within the path's stats or tags arrays, it returns the item name
+        let tags = path.tags
+        let newTagArray = [];
+    
+        if (!type) {
+            let stats = Object.keys(path.stats);
+            for (entry of stats) {newTagArray.push(entry);}
+        }
+        if (type != "prime") {
+            let tags = path.tags
+            for (entry of tags) {newTagArray.push(entry);}
+        }
+        else {
+            let stats = Object.keys(path.primeStats);
+            for (entry of stats) {newTagArray.push(entry);}
+        }
+    
+        let itemFound = false;
+    
+        for (entry of newTagArray) {
+            if (entry === statistic) {itemFound = true;}
+        }
+    
+        return itemFound ? `${name}<br>` : "";
+    },
+    returnItemsWithStat(statistic) {
+        //This is a function to go over every possible equipped item, check if it the item can at any point provide the target, and if so, return the item names.
+        let htmlString = "";
+        let globalReference = globalRecords;
+        let accessoryReference = globalReference.accessories;
+        let consumableReference = globalReference.consumables;
+        let weaponReference = globalReference.weapons;
+        let armorReference = globalReference.armor;
+        let traitReference = globalReference.greatTraitRecords
+        let classReference = globalReference.archs;
+    
+        htmlString += tooltips.checkItemForStat(amulets[accessoryReference.amulet],accessoryReference.amulet,statistic);
+        for (let i=1;i<=4;i++) {
+            htmlString += tooltips.checkItemForStat(rings[accessoryReference[`ring${i}`]],accessoryReference[`ring${i}`],statistic);
+        }
+        htmlString += tooltips.checkItemForStat(relics[accessoryReference.relic],accessoryReference.relic,statistic);
+        for (let i=1;i<=3;i++) {
+            htmlString += tooltips.checkItemForStat(fragments[accessoryReference[`fragment${i}`]],accessoryReference[`fragment${i}`],statistic);
+        }
+    
+        for (let i=1;i<=7;i++) {
+            htmlString += tooltips.checkItemForStat(concoctions[consumableReference[`concoction${i}`]],consumableReference[`concoction${i}`],statistic);
+        }
+        for (let i=1;i<=4;i++) {
+            htmlString += tooltips.checkItemForStat(quickUses[consumableReference[`quickUse${i}`]],consumableReference[`quickUse${i}`],statistic);
+        }
+        htmlString += tooltips.checkItemForStat(primary[weaponReference.primary],weaponReference.primary,statistic);
+        htmlString += tooltips.checkItemForStat(rangedMutators[weaponReference.primaryMutator],weaponReference.primaryMutator,statistic);
+        htmlString += primary[weaponReference.primary].builtIN ? tooltips.checkItemForStat(builtInPrimary[primary[weaponReference.primary].builtIN],primary[weaponReference.primary].builtIN,statistic)
+        : tooltips.checkItemForStat(rangedMods[weaponReference.primaryMod],weaponReference.primaryMod,statistic);
+        htmlString += tooltips.checkItemForStat(melee[weaponReference.melee],weaponReference.melee,statistic);
+        htmlString += tooltips.checkItemForStat(meleeMutators[weaponReference.meleeMutator],weaponReference.meleeMutator,statistic);
+        htmlString += melee[weaponReference.melee].builtIN ? tooltips.checkItemForStat(builtInMelee[melee[weaponReference.melee].builtIN],melee[weaponReference.melee].builtIN,statistic)
+        : "";
+        htmlString += tooltips.checkItemForStat(secondary[weaponReference.secondary],weaponReference.secondary,statistic);
+        htmlString += tooltips.checkItemForStat(rangedMutators[weaponReference.secondaryMutator],weaponReference.secondaryMutator,statistic);
+        htmlString += secondary[weaponReference.secondary].builtIN ? tooltips.checkItemForStat(builtInSecondary[secondary[weaponReference.secondary].builtIN],secondary[weaponReference.secondary].builtIN,statistic)
+        : tooltips.checkItemForStat(rangedMods[weaponReference.secondaryMod],weaponReference.secondaryMod,statistic);
+        htmlString += tooltips.checkItemForStat(helmets[armorReference.helmet],armorReference.helmet,statistic);
+        htmlString += tooltips.checkItemForStat(chests[armorReference.chest],armorReference.chest,statistic);
+        htmlString += tooltips.checkItemForStat(legs[armorReference.leg],armorReference.leg,statistic);
+        htmlString += tooltips.checkItemForStat(hands[armorReference.hand],armorReference.hand,statistic);
+    
+        for (let i=0;i<traitReference.length;i++) {
+            htmlString += tooltips.checkItemForStat(traits[traitReference[i].name],traitReference[i].name,statistic,"trait");
+        }
+    
+    
+        htmlString += tooltips.checkItemForStat(classInfo[classReference.one.class],classInfo[classReference.one.class].primePerk,statistic,"prime");
+        htmlString += tooltips.checkItemForStat(classInfo[classReference.one.class].abilities[classReference.one.ability],classReference.one.ability,statistic);
+        for (let i=1;i<=4;i++) {
+            htmlString += tooltips.checkItemForStat(classInfo[classReference.one.class].passives[`passive${i}`],classInfo[classReference.one.class].passives[`passive${i}`].name,statistic);
+        }
+    
+        htmlString += tooltips.checkItemForStat(classInfo[classReference.two.class].abilities[classReference.two.ability],classReference.two.ability,statistic);
+        for (let i=1;i<=4;i++) {
+            htmlString += tooltips.checkItemForStat(classInfo[classReference.two.class].passives[`passive${i}`],classInfo[classReference.two.class].passives[`passive${i}`].name,statistic);
+        }
+    
+        // classReference
+    
+    
+        return htmlString;
+    },
     updateTooltipDisplay(elemID,breakdownString,statArray) {
         let listItemsHeader = "<div>The following selections can contribute to this statistic:</div>"
         let statsString = "";
 
         for (let i=0;i<statArray.length;i++) {
-            statsString += returnItemsWithStat(statArray[i])
+            statsString += tooltips.returnItemsWithStat(statArray[i])
         }
 
         tooltipStorage[elemID] =
