663388580992540e9c4a61ee7a7688fe6c8291c0
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2025-02-06 10:45:25 -0700

    Erosion Stat Page update

---
+++ TFD/Calculator/parsedErosionData.js
@@ -0,0 +1,112 @@
+let erosionModsData = {
+  "21": {
+    "Firearm Chill%": "6.39",
+    "Firearm Fire%": "6.2",
+    "Firearm Electric%": "6.2",
+    "Firearm Toxin%": "6.2",
+    "Chill Power": "0.15",
+    "Non-Attribute Power": "0.13",
+    "Fire Power": "0.13",
+    "Electric Power": "0.13",
+    "Toxic Power": "0.13"
+  },
+  "22": {
+    "Firearm Toxin%": "5.68",
+    "Firearm Fire%": "5.3",
+    "Firearm Chill%": "5.3",
+    "Firearm Electric%": "5.3",
+    "Toxic Power": "0",
+    "Non-Attribute Power": "-0.04",
+    "Fire Power": "-0.04",
+    "Chill Power": "-0.04",
+    "Electric Power": "-0.04"
+  },
+  "23": {
+    "Firearm Toxin%": "4.97",
+    "Firearm Fire%": "4.4",
+    "Firearm Chill%": "4.4",
+    "Firearm Electric%": "4.4",
+    "Toxic Power": "-0.15",
+    "Non-Attribute Power": "-0.21",
+    "Fire Power": "-0.21",
+    "Chill Power": "-0.21",
+    "Electric Power": "-0.21"
+  },
+  "24": {
+    "Firearm Electric%": "4.26",
+    "Firearm Fire%": "3.5",
+    "Firearm Chill%": "3.5",
+    "Firearm Toxin%": "3.5",
+    "Electric Power": "-0.3",
+    "Non-Attribute Power": "-0.38",
+    "Fire Power": "-0.38",
+    "Chill Power": "-0.38",
+    "Toxic Power": "-0.38"
+  },
+  "25": {
+    "Firearm Chill%": "3.55",
+    "Firearm Fire%": "2.6",
+    "Firearm Electric%": "2.6",
+    "Firearm Toxin%": "2.6",
+    "Chill Power": "-0.45",
+    "Non-Attribute Power": "-0.55",
+    "Fire Power": "-0.55",
+    "Electric Power": "-0.55",
+    "Toxic Power": "-0.55"
+  },
+  "26": {
+    "Firearm Chill%": "2.84",
+    "Firearm Fire%": "1.7",
+    "Firearm Electric%": "1.7",
+    "Firearm Toxin%": "1.7",
+    "Chill Power": "-0.6",
+    "Non-Attribute Power": "-0.72",
+    "Fire Power": "-0.72",
+    "Electric Power": "-0.72",
+    "Toxic Power": "-0.72"
+  },
+  "27": {
+    "Firearm Toxin%": "2.13",
+    "Firearm Fire%": "0.8",
+    "Firearm Chill%": "0.8",
+    "Firearm Electric%": "0.8",
+    "Toxic Power": "-0.75",
+    "Non-Attribute Power": "-0.89",
+    "Fire Power": "-0.89",
+    "Chill Power": "-0.89",
+    "Electric Power": "-0.89"
+  },
+  "28": {
+    "Firearm Electric%": "1.42",
+    "Firearm Fire%": "-0.1",
+    "Firearm Chill%": "-0.1",
+    "Firearm Toxin%": "-0.1",
+    "Electric Power": "-0.9",
+    "Non-Attribute Power": "-1.06",
+    "Fire Power": "-1.06",
+    "Chill Power": "-1.06",
+    "Toxic Power": "-1.06"
+  },
+  "29": {
+    "Firearm Electric%": "0.71",
+    "Firearm Fire%": "-1",
+    "Firearm Chill%": "-1",
+    "Firearm Toxin%": "-1",
+    "Electric Power": "-1.05",
+    "Non-Attribute Power": "-1.23",
+    "Fire Power": "-1.23",
+    "Chill Power": "-1.23",
+    "Toxic Power": "-1.23"
+  },
+  "30": {
+    "Firearm Chill%": "0",
+    "Firearm Fire%": "-1.9",
+    "Firearm Electric%": "-1.9",
+    "Firearm Toxin%": "-1.9",
+    "Chill Power": "-1.2",
+    "Non-Attribute Power": "-1.4",
+    "Fire Power": "-1.4",
+    "Electric Power": "-1.4",
+    "Toxic Power": "-1.4"
+  }
+}
