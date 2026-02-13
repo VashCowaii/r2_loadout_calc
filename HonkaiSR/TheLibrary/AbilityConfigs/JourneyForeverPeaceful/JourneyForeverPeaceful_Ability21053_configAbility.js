dec1cd431c19db64c059cbd74405a9e0b97c13a2
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-02-12 22:43:39 -0700

    update journeyFP

--- HonkaiSR/TheLibrary/AbilityConfigs/JourneyForeverPeaceful/JourneyForeverPeaceful_Ability21053_configAbility.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/JourneyForeverPeaceful/JourneyForeverPeaceful_Ability21053_configAbility.js
@@ -203,7 +203,7 @@ const configAbility = {
     }
   ],
   "isLightcone": true,
-  "desc": "Increases the wearer's provided Shield Effect by #1[i]% and while any ally target has a Shield, the wearer's DMG dealt increases by #2[i]%.",
+  "desc": "Increases the wearer's provided Shield Effect by #1[i]%. While an ally target has a Shield, their DMG dealt increases by #2[i]%.",
   "params": [
     [
       0.12,
