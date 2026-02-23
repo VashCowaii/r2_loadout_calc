0c6a46b195f26e49e86a86a554ab58204c3109d9
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-02-22 22:42:16 -0700

    hsr allowed calc char list

---
+++ HonkaiSR/TheLibrary/stats4CalcAllowedChars.js
@@ -0,0 +1,13 @@
+global.calcAllowedSet = new Set([
+    "Silver Wolf",
+    "Bronya",
+    "Tingyun",
+    "Huohuo",
+    "Gallagher",
+    "Robin",
+    "Sunday",
+    "Tribbie",
+    "Dan Heng • Permansor Terrae",
+    "Trailblazer - Remembrance",
+    "Saber",
+]);
