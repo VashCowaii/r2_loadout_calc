3bc7509b5d0f3fc4c30031f1014ff394ad23486c
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-02-28 22:48:02 -0700

    hsr calc trim reused display warnings

--- HonkaiSR/Calculator/testerValues.js
+++ HonkaiSR/Calculator/testerValues.js
@@ -164,6 +164,10 @@ const customDisplayValuesLog = {
         {valueName: "Divine Provision Active", refName: "talentProvisionIsActive", isCharacterState: true},
         // {valueName: "Clease Remaining", refName: "cleanseCounter"},
     ],
+    "Luocha": [
+        // {valueName: "Divine Provision Active", refName: "talentProvisionIsActive", isCharacterState: true},
+        // {valueName: "Clease Remaining", refName: "cleanseCounter"},
+    ],
     "Tribbie": [
         {valueName: "Numinosity Active", refName: "numinosityIsActive", isCharacterState: true},
         {valueName: "Ult Zone Active", refName: "tribbieZoneActive", isCharacterState: true},
@@ -193,7 +197,21 @@ const permaConditionsTextLibrary = {
     "archerSub5casts": "Skill Use Count < 5",
 }
 
+const defaultStandardAbilityDisplayWarnings = {
+    hasEnhancedState: false,
+    "Skill": "",
+    "Ultimate": "",
+    "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
+    "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
+};
 const conditionsCharacterDisplayWarning = {
+    "defaultStandardWarnings": {
+        hasEnhancedState: false,
+        "Skill": "",
+        "Ultimate": "",
+        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
+        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
+    },
     "Saber": {
         hasEnhancedState: true,
         "Skill": "Skill conditions are ignored when Saber's Basic ATK is enhanced.",
@@ -210,15 +228,7 @@ const conditionsCharacterDisplayWarning = {
         "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
         "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
     },
-
-    "Sunday": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
+    "Sunday": defaultStandardAbilityDisplayWarnings,
     "Bronya": {
         hasEnhancedState: false,
         "Skill": "",
@@ -243,48 +253,13 @@ const conditionsCharacterDisplayWarning = {
         "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
         "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxedCyrene]
     },
-    "Tribbie": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Robin": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Silver Wolf": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Tingyun": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-
-    "Dan Heng • Permansor Terrae": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
+    "Tribbie": defaultStandardAbilityDisplayWarnings,
+    "Robin": defaultStandardAbilityDisplayWarnings,
+    "Silver Wolf": defaultStandardAbilityDisplayWarnings,
+    "Tingyun": defaultStandardAbilityDisplayWarnings,
+    "Dan Heng • Permansor Terrae": defaultStandardAbilityDisplayWarnings,
     "Aglaea": {
-        hasEnhancedState: false,
+        hasEnhancedState: true,
         "Skill": "",
         "Ultimate": "",
         "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
@@ -297,70 +272,16 @@ const conditionsCharacterDisplayWarning = {
         "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
         "UltimatePermaConditions": [permaConditionsTextLibrary.energyHalf]
     },
-    "Aventurine": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Kafka": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Hysilens": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Pela": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Asta": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Ruan Mei": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Lynx": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Topaz & Numby": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
-    "Anaxa": {
-        hasEnhancedState: false,
-        "Skill": "",
-        "Ultimate": "",
-        "SkillPermaConditions": [permaConditionsTextLibrary.atLeast1SP,],
-        "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
-    },
+    "Aventurine": defaultStandardAbilityDisplayWarnings,
+    "Kafka": defaultStandardAbilityDisplayWarnings,
+    "Hysilens": defaultStandardAbilityDisplayWarnings,
+    "Pela": defaultStandardAbilityDisplayWarnings,
+    "Asta": defaultStandardAbilityDisplayWarnings,
+    "Ruan Mei": defaultStandardAbilityDisplayWarnings,
+    "Lynx": defaultStandardAbilityDisplayWarnings,
+    "Topaz & Numby": defaultStandardAbilityDisplayWarnings,
+    "Anaxa": defaultStandardAbilityDisplayWarnings,
+    "Huohuo": defaultStandardAbilityDisplayWarnings,
     "Archer": {
         hasEnhancedState: false,
         "Skill": "",
@@ -368,6 +289,7 @@ const conditionsCharacterDisplayWarning = {
         "SkillPermaConditions": [permaConditionsTextLibrary.atLeast2SP,permaConditionsTextLibrary.archerSub5casts],
         "UltimatePermaConditions": [permaConditionsTextLibrary.energyMaxed]
     },
+    "Luocha": defaultStandardAbilityDisplayWarnings,
 }
 
 
@@ -1239,6 +1161,14 @@ const defaultConditions = {
 
     //ABUNDANCE
     "Huohuo": {
+        hasEnhancedState: false,
+        "Skill": null,
+        "Ultimate": {
+            type: "AND",
+            array: []
+        }
+    },
+    "Luocha": {
         hasEnhancedState: false,
         "Skill": null,
         "Ultimate": {
