561143651fb32cfd64329e474dbb089db172d80c
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-12 17:01:51 -0600

    hsr library kill data warning

--- HonkaiSR/TheLibrary/vashWarning.js
+++ HonkaiSR/TheLibrary/vashWarning.js
@@ -1,4 +1,5 @@
-let currentWarning = "NOTE:<br>The missing hp amounts AFTER phase 1, on 4.0 enemies, should be resolved soonTM.";
+// let currentWarning = "NOTE:<br>The missing hp amounts AFTER phase 1, on 4.0 enemies, should be resolved soonTM.";
+let currentWarning = "";
 
 if (currentWarning) {readSelection("vashCustomWarningNote").innerHTML = currentWarning;}
 else {readSelection("vashCustomWarningNote").style.display = "none";}
