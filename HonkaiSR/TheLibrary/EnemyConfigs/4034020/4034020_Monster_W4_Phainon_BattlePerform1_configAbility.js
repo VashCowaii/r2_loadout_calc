fa6fa0852edc83b36c970815af9810a2d234cf58
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-12 19:21:41 -0600

    eventreader 4034020

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/4034020/4034020_Monster_W4_Phainon_BattlePerform1_configAbility.js
@@ -0,0 +1,16 @@
+const configAbility = {
+  "fileName": "4034020_Monster_W4_Phainon_BattlePerform1",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Force Entity Death",
+      "target": {
+        "name": "Target Name",
+        "target": "{{Caster's Minions}}"
+      }
+    }
+  ],
+  "references": []
+}
