58dedd1a342fec5eec9d2dbde6b129b563cb6665
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-14 16:39:26 -0600

    hsr enemy grafSlacker 1&2

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012041/5012041_Modifiers.js
@@ -0,0 +1,17 @@
+const configAbility = {
+  "fileName": "5012041_Modifiers",
+  "abilityType": "Char. Modifiers",
+  "energy": null,
+  "toughnessList": [
+    0,
+    0,
+    0
+  ],
+  "parse": [
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-323792710\">Enemy_W2_Mecha01_03_Resistance_Ice</a>"
+    }
+  ],
+  "references": []
+}
--- HonkaiSR/TheLibrary/EnemyConfigs/5012041/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012041/compositeAbilityObject.js
@@ -1,2 +1,250 @@
 const entityPageType = "enemy"
-let compositeAbilityObject = []
+const compositeAbilityObject = {
+  "fullCharacterName": 5012041,
+  "trimCharacterName": 5012041,
+  "abilityList": [
+    "5012041_Monster_W2_Mecha01_03_Passive",
+    "5012041_Monster_W2_Mecha01_03_Ability01_Part02",
+    "5012041_Monster_W2_Mecha01_03_Ability01_Part01",
+    "5012041_Modifiers"
+  ],
+  "abilityObject": {
+    "5012041_Monster_W2_Mecha01_03_Passive": {
+      "fileName": "5012041_Monster_W2_Mecha01_03_Passive",
+      "skillTrigger": "PassiveSkill02",
+      "abilityType": "Talent",
+      "energy": null,
+      "toughnessList": null,
+      "parse": [
+        {
+          "name": "IF",
+          "conditions": {
+            "name": "Check Boolean Value",
+            "target": {
+              "name": "Target Name",
+              "target": "{{Caster}}"
+            },
+            "value": "IceResistance"
+          },
+          "passed": [
+            {
+              "name": "Add Events/Bonuses",
+              "to": {
+                "name": "Target Name",
+                "target": "{{Caster}}"
+              },
+              "modifier": "<a class=\"gModGreen\" id=\"-391821356\">W2_Mecha01_03_Resistance_Ice</a>"
+            }
+          ]
+        }
+      ],
+      "references": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-391821356\">W2_Mecha01_03_Resistance_Ice</a>",
+          "execute": [
+            {
+              "eventTrigger": "Action Choice Window [Anyone]",
+              "execute": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "AND",
+                    "conditionList": [
+                      {
+                        "name": "Compare: Variable",
+                        "value1": "Appear_Flag",
+                        "compareType": "=",
+                        "value2": 0
+                      },
+                      {
+                        "name": "Compare: Variable",
+                        "value1": "Trigger_Flag",
+                        "compareType": "=",
+                        "value2": 0
+                      }
+                    ]
+                  },
+                  "passed": [
+                    {
+                      "name": "Define Custom Variable",
+                      "variableName": "Trigger_Flag",
+                      "value": 1
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "eventTrigger": "Take Damage End [Owner]: Hit",
+              "execute": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Is Damage Type/Element",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "list": [
+                      "Ice"
+                    ]
+                  }
+                }
+              ]
+            },
+            {
+              "eventTrigger": "Enter Battle",
+              "execute": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "AND",
+                    "conditionList": [
+                      {
+                        "name": "Compare: Variable",
+                        "value1": "Appear_Flag",
+                        "compareType": "=",
+                        "value2": 0
+                      },
+                      {
+                        "name": "Compare: Variable",
+                        "value1": "Trigger_Flag",
+                        "compareType": "=",
+                        "value2": 0
+                      },
+                      {
+                        "name": "Compare: Variable",
+                        "value1": "Wave Count",
+                        "compareType": "=",
+                        "value2": 1
+                      }
+                    ]
+                  },
+                  "passed": [
+                    {
+                      "name": "Define Custom Variable",
+                      "variableName": "Trigger_Flag",
+                      "value": 1
+                    }
+                  ]
+                }
+              ]
+            }
+          ],
+          "stackData": [],
+          "latentQueue": []
+        }
+      ]
+    },
+    "5012041_Monster_W2_Mecha01_03_Ability01_Part02": {
+      "fileName": "5012041_Monster_W2_Mecha01_03_Ability01_Part02",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [
+        "Ability Start",
+        {
+          "name": "ATK Scaling DMG",
+          "target": {
+            "name": "Target Name",
+            "target": "{{Ability Target(ST)}}"
+          },
+          "AttackScaling": {
+            "DamageType": "Ice",
+            "Damage": {
+              "operator": "Variables[0] ({[Skill01[0]]}) || RETURN",
+              "displayLines": "{[Skill01[0]]}",
+              "constants": [],
+              "variables": [
+                "{[Skill01[0]]}"
+              ]
+            },
+            "Toughness": null,
+            "Tags": null,
+            "EnergyGainPercent": "100%"
+          }
+        },
+        "Trigger: Attack End",
+        {
+          "name": "Add Events/Bonuses",
+          "to": {
+            "name": "Target Name",
+            "target": "{{Ability Target(ST)}}"
+          },
+          "modifier": "<a class=\"gModGreen\" id=\"-43160834\">Standard_AllDamageTypeTakenRatio</a>[<span class=\"descriptionNumberColor\">Vulnerability</span>]",
+          "duration": {
+            "operator": "Variables[0] ({[Skill01[3]]}) || RETURN",
+            "displayLines": "{[Skill01[3]]}",
+            "constants": [],
+            "variables": [
+              "{[Skill01[3]]}"
+            ]
+          },
+          "baseChance": {
+            "operator": "Variables[0] ({[Skill01[1]]}) || RETURN",
+            "displayLines": "{[Skill01[1]]}",
+            "constants": [],
+            "variables": [
+              "{[Skill01[1]]}"
+            ]
+          },
+          "valuePerStack": {
+            "MDF_PropertyValue": {
+              "operator": "Variables[0] ({[Skill01[2]]}) || RETURN",
+              "displayLines": "{[Skill01[2]]}",
+              "constants": [],
+              "variables": [
+                "{[Skill01[2]]}"
+              ]
+            }
+          }
+        },
+        "Trigger: Ability End"
+      ],
+      "references": []
+    },
+    "5012041_Monster_W2_Mecha01_03_Ability01_Part01": {
+      "fileName": "5012041_Monster_W2_Mecha01_03_Ability01_Part01",
+      "childAbilityList": [
+        "5012041_Monster_W2_Mecha01_03_Ability01_Camera",
+        "5012041_Monster_W2_Mecha01_03_Ability01_Part01",
+        "5012041_Monster_W2_Mecha01_03_Ability01_Part02"
+      ],
+      "skillTrigger": "Skill01",
+      "abilityType": "Skill",
+      "energy": null,
+      "toughnessList": null,
+      "parse": [
+        {
+          "name": "Trigger Ability",
+          "from": {
+            "name": "Target Name",
+            "target": "{{Caster}}"
+          },
+          "ability": "Monster_W2_Mecha01_03_Ability01_Part02",
+          "isTrigger": true
+        },
+        "Deleted bullshit"
+      ],
+      "references": []
+    },
+    "5012041_Modifiers": {
+      "fileName": "5012041_Modifiers",
+      "abilityType": "Char. Modifiers",
+      "energy": null,
+      "toughnessList": [
+        0,
+        0,
+        0
+      ],
+      "parse": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-323792710\">Enemy_W2_Mecha01_03_Resistance_Ice</a>"
+        }
+      ],
+      "references": []
+    }
+  }
+}
