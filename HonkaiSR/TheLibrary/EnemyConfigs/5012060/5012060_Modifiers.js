8f5254966f2da6e54399c35b3d6ee7516892efae
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-14 16:40:29 -0600

    hsr enemy grafDem 1&2

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012060/5012060_Modifiers.js
@@ -0,0 +1,17 @@
+const configAbility = {
+  "fileName": "5012060_Modifiers",
+  "abilityType": "Char. Modifiers",
+  "energy": null,
+  "toughnessList": [
+    0,
+    0,
+    0
+  ],
+  "parse": [
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-1707357339\">Enemy_W2_Mecha01_05_Resistance_Thunder</a>"
+    }
+  ],
+  "references": []
+}
