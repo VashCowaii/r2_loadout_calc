ecd1f4a0e24459bad5f8b6ccf383cb964c50cefa
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-25 02:21:40 -0600

    hsr library update ichor2

--- HonkaiSR/TheLibrary/EnemyConfigs/4063010/4063010_Monster_W4_Serpent_Ability04_Part02_configAbility.js
+++ HonkaiSR/TheLibrary/EnemyConfigs/4063010/4063010_Monster_W4_Serpent_Ability04_Part02_configAbility.js
@@ -236,6 +236,27 @@ const configAbility = {
       },
       "modifier": "<a class=\"gModGreen\" id=\"-919299430\">Enemy_W4_Serpent_Charge</a>[<span class=\"descriptionNumberColor\">Whirl</span>]"
     },
+    {
+      "name": "IF",
+      "conditions": {
+        "name": "Check Boolean Value",
+        "target": {
+          "name": "Target Name",
+          "target": "{{Caster}}"
+        },
+        "value": "ENEMIES_OBJECT_UNUSED__181"
+      },
+      "passed": [
+        {
+          "name": "Remove Events/Bonuses",
+          "to": {
+            "name": "Target Name",
+            "target": "{{Caster}}"
+          },
+          "modifier": "<a class=\"gModGreen\" id=\"437668983\">Monster_W4_Nikadory_RLBoss_DamageUp</a>[<span class=\"descriptionNumberColor\">DMG Boost</span>]"
+        }
+      ]
+    },
     "Trigger: Ability End"
   ],
   "references": []
