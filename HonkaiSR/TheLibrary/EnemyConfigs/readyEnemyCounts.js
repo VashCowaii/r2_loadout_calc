aac8fdd6716ac239d8a166e395f7f66a3eb7b5e5
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-26 21:04:09 -0600

    hsr library update readycount/leaderboard

--- HonkaiSR/TheLibrary/EnemyConfigs/readyEnemyCounts.js
+++ HonkaiSR/TheLibrary/EnemyConfigs/readyEnemyCounts.js
@@ -1,6 +1,6 @@
 let readyEnemyCounts = {
   "totalEnemyCount": 560,
   "totalAllEnemyCount": 2431,
-  "readyEnemyCount": 552,
-  "readyMaskCount": 2413
+  "readyEnemyCount": 557,
+  "readyMaskCount": 2419
 }
