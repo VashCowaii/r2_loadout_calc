e889647c17e3f50fbca5af863b3455edb603571c
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-03-31 15:13:45 -0600

    60020

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/60020/60020_BattleEventAbility_60020_Camera_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "60020_BattleEventAbility_60020_Camera",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Ability Target List}}"
+  }
+}
---
+++ HonkaiSR/TheLibrary/AbilityConfigs/60020/60020_MissionBattleEvent60020_Ability03_Part01_configAbility.js
@@ -0,0 +1,80 @@
+const configAbility = {
+  "fileName": "60020_MissionBattleEvent60020_Ability03_Part01",
+  "childAbilityList": [
+    "60020_MissionBattleEvent60020_Ability03_Part01",
+    "60020_MissionBattleEvent60020_Ability03_Part02",
+    "60020_BattleEventAbility_60020_Camera"
+  ],
+  "skillTrigger": "Skill03",
+  "abilityType": "Ultimate",
+  "energy": null,
+  "toughnessList": [
+    0,
+    0,
+    0
+  ],
+  "parse": [
+    "Deleted bullshit",
+    {
+      "name": "Trigger Ability",
+      "from": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "ability": "MissionBattleEvent60020_Ability03_Part02",
+      "isTrigger": true
+    },
+    {
+      "name": "IF",
+      "conditions": {
+        "name": "Compare: Variable",
+        "value1": "_Count",
+        "compareType": ">",
+        "value2": 2
+      },
+      "failed": [
+        {
+          "name": "IF",
+          "conditions": {
+            "name": "Compare: Variable",
+            "value1": "_Count",
+            "compareType": "=",
+            "value2": 2
+          },
+          "failed": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "Compare: Variable",
+                "value1": "_Count",
+                "compareType": "=",
+                "value2": 1
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Define Custom Variable",
+      "variableName": "_Count",
+      "value": {
+        "operator": "Variables[0] (_Count) || Constants[0] (1) || ADD || RETURN",
+        "displayLines": "(_Count + 1)",
+        "constants": [
+          1
+        ],
+        "variables": [
+          "_Count"
+        ]
+      }
+    }
+  ],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Hostile Entities(AOE)}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "All Enemy Team"
+  }
+}
