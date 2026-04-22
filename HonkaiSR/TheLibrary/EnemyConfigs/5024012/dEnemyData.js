8fc2cb58eedc941a584ef1489c0a0505d711d94b
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-04-21 18:42:33 -0600

    new enemy pages

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5024012/dEnemyData.js
@@ -0,0 +1,69 @@
+let enemyData = {
+  "name": "Paramount Bliss Inverted: Illwish Archlotus",
+  "image": 5024012,
+  "bar": 3,
+  "bodyType": "LargeMonster",
+  "hitClass": "B",
+  "rank": "LittleBoss",
+  "stArray": [
+    18,
+    210,
+    1395,
+    144,
+    null,
+    0.2,
+    0.3,
+    0.5,
+    720,
+    null,
+    1
+  ],
+  "options": {
+    "5024012": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Physical",
+        "Thunder",
+        "Imaginary"
+      ],
+      "res": {
+        "Fire": 0.4,
+        "Ice": 0.4,
+        "Wind": 0.4,
+        "Quantum": 0.4
+      },
+      "resMod": {
+        "STAT_CTRL": 0.5
+      },
+      "abilities": [
+        2703,
+        2704,
+        2705,
+        2706,
+        2707,
+        2708,
+        2709
+      ],
+      "enemyTag": [
+        "GridFightBoss"
+      ],
+      "summons": [
+        {
+          "name": "Illwish Lotus",
+          "over": 5022010,
+          "base": 5022010
+        }
+      ]
+    }
+  }
+}
