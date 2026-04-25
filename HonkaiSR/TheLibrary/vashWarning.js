f4160bd7111fd903304f7f30d313538c7dd62ce5
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-04-24 23:35:37 -0600

    update progress warning

--- HonkaiSR/TheLibrary/vashWarning.js
+++ HonkaiSR/TheLibrary/vashWarning.js
@@ -1,5 +1,5 @@
 // let currentWarning = "NOTE:<br>The missing hp amounts AFTER phase 1, on 4.0 enemies, should be resolved soonTM.";
-let currentWarning = "4.2 is IN PROGRESS<br><br>See changelog for progress.";
+let currentWarning = "4.2 is IN PROGRESS<br><br>Enemies should all be done, only some new character eventreaders remain.";
 
 if (currentWarning) {readSelection("vashCustomWarningNote").innerHTML = currentWarning;}
 else {readSelection("vashCustomWarningNote").style.display = "none";}
