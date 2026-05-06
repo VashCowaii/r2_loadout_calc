a414a7638fd6969a58657cd1d23e0b5e8f12e5e4
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-06 09:08:03 -0600

    61002

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/61002/61002_ActionEventAbility_Camera_AllLightTeam_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "61002_ActionEventAbility_Camera_AllLightTeam",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "Inherent Target"
+  }
+}
