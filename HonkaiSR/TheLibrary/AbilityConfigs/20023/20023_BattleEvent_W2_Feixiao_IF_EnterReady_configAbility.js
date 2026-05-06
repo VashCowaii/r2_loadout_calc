901fcbe9eac7526a648ea952c34b9fc8f7f7fe84
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-06 08:48:50 -0600

    20023

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/20023/20023_BattleEvent_W2_Feixiao_IF_EnterReady_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "20023_BattleEvent_W2_Feixiao_IF_EnterReady",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Hostile Entities(AOE)}}"
+  }
+}
