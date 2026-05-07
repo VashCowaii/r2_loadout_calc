defd8bcc6183958366b4f593e6de13203cdcce62
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:58:43 -0600

    30133

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30133/30133_BattleEventAbility_Challenge_Deathrattle_Camera_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "30133_BattleEventAbility_Challenge_Deathrattle_Camera",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "Inherent Target"
+  }
+}
