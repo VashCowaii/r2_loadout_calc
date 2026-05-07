76505f1c69558efb591eef468f0384f83539a797
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 00:54:31 -0600

    101

--- HonkaiSR/TheLibrary/AbilityConfigs/101/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/101/compositeAbilityObject.js
@@ -2,9 +2,88 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 101,
   "trimCharacterName": 101,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "101_BattleEventAbility_900101"
+  ],
+  "abilityObject": {
+    "101_BattleEventAbility_900101": {
+      "fileName": "101_BattleEventAbility_900101",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "whenAdded": [
+        {
+          "name": "Add Events/Bonuses",
+          "to": {
+            "name": "Target Name",
+            "target": "{{Caster}}"
+          },
+          "modifier": "<a class=\"gModGreen\" id=\"-2005310229\">Modifier_BattleEventAbility_900101</a>"
+        }
+      ],
+      "references": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-2005310229\">Modifier_BattleEventAbility_900101</a>",
+          "execute": [
+            {
+              "eventTrigger": "Turn [Pre-action Phase]",
+              "execute": [
+                {
+                  "name": "Add Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1166335492\">Challenge_Camera_AllTeam_Temp_1</a>"
+                },
+                {
+                  "name": "UI Display Event"
+                },
+                {
+                  "name": "Add Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1216668349\">Challenge_Camera_AllTeam_Temp_2</a>"
+                },
+                {
+                  "name": "Action Advance/Delay",
+                  "target": {
+                    "name": "Target Name",
+                    "target": "{{Player Team All}}"
+                  },
+                  "advanceType": "Advance",
+                  "multiAdd": -1
+                },
+                {
+                  "name": "ATK Scaling DMG",
+                  "target": {
+                    "name": "Target Name",
+                    "target": "{{Enemy Team All}}"
+                  },
+                  "AttackScaling": {
+                    "DamageType": "Wind",
+                    "Damage": {
+                      "displayLines": 0.3
+                    },
+                    "Toughness": null,
+                    "ToughnessDMGType": {
+                      "DamageType": "Wind"
+                    },
+                    "Tags": null,
+                    "attackType": "Follow-up"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
