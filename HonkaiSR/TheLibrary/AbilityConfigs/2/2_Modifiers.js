31534bfb3842f7b340a4fe9eb71cffd472bbc2c9
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 01:20:37 -0600

    2

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/2/2_Modifiers.js
@@ -0,0 +1,252 @@
+const configAbility = {
+  "fileName": "2_Modifiers",
+  "abilityType": "Char. Modifiers",
+  "energy": null,
+  "toughnessList": [
+    0,
+    0,
+    0
+  ],
+  "parse": [
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-110303717\">JingYuan_Tree03_CriticalChanceUp</a>[<span class=\"descriptionNumberColor\">CRIT Rate Boost</span>]",
+      "stackType": "ReplaceByCaster",
+      "description": "CRIT Rate +<span class=\"descriptionNumberColor\">MDF_PropertyValue</span>.",
+      "type": "Buff",
+      "effectName": "CRIT Rate Boost",
+      "statusName": "CRIT Rate Boost",
+      "execute": [
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier",
+          "execute": [
+            {
+              "name": "Stack Target Stat Value",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "statName": "&nbsp;<span class=\"descriptionNumberColor\">CritRateBase</span>&nbsp;",
+              "value": {
+                "operator": "Variables[0] (MDF_PropertyValue) || RETURN",
+                "displayLines": "MDF_PropertyValue",
+                "constants": [],
+                "variables": [
+                  "MDF_PropertyValue"
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-1778616310\">JingYuan_CriticalDamageUp</a>",
+      "stackType": "ReplaceByCaster",
+      "execute": [
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier",
+          "execute": [
+            {
+              "name": "Stack Target Stat Value",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "statName": "&nbsp;<span class=\"descriptionNumberColor\">CritDamageBase</span>&nbsp;",
+              "value": {
+                "operator": "Variables[0] (MDF_PropertyValue) || RETURN",
+                "displayLines": "MDF_PropertyValue",
+                "constants": [],
+                "variables": [
+                  "MDF_PropertyValue"
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__767237660\">JingYuan_Eidolon6_AllDamageTypeTakenRatio</a>",
+      "stackType": "ReplaceByCaster",
+      "lifeCyclePhaseAllowed": "ModifierPhase1End",
+      "addStacksPerTrigger": 1,
+      "execute": [
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier",
+          "execute": [
+            {
+              "name": "Define Custom Variable with Modifier Values",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "valueType": "Layer",
+              "variableName": "MDF_Layer",
+              "modifierName": "<a class=\"gModGreen\" id=\"767237660\">JingYuan_Eidolon6_AllDamageTypeTakenRatio</a>",
+              "multiplier": 1
+            },
+            {
+              "name": "Stack Target Stat Value",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "statName": "&nbsp;<span class=\"descriptionNumberColor\">Vulnerability</span>&nbsp;",
+              "value": {
+                "operator": "Variables[0] (MDF_PropertyValue) || Variables[1] (MDF_Layer) || MUL || RETURN",
+                "displayLines": "(MDF_PropertyValue * MDF_Layer)",
+                "constants": [],
+                "variables": [
+                  "MDF_PropertyValue",
+                  "MDF_Layer"
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__1088669628\">JingYuan_Eidolon2_DamageAddedRatio</a>[<span class=\"descriptionNumberColor\">Dharma Corpora</span>]",
+      "stackType": "ReplaceByCaster",
+      "description": "Jing Yuan's Basic ATK, Skill, and Ultimate deal <span class=\"descriptionNumberColor\">MDF_DamageAddedRatio</span> increased DMG.",
+      "type": "Buff",
+      "statusName": "Dharma Corpora",
+      "execute": [
+        {
+          "eventTrigger": "Deal Damage Start [Owner]: Hit",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "Attack Type",
+                "attackTypes": [
+                  "Basic ATK",
+                  "Skill",
+                  "Ultimate"
+                ]
+              },
+              "passed": [
+                {
+                  "name": "Adjust Target Stats",
+                  "modifiedValuesArray": [
+                    {
+                      "on": "Attacker",
+                      "statName": "&nbsp;<span class=\"descriptionNumberColor\">DamageAll</span>&nbsp;",
+                      "value": "MDF_DamageAddedRatio"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-1762822846\">JingYuan_BattleEvent_SpeedUp</a>",
+      "stackType": "ReplaceByCaster",
+      "modifierFlags": [
+        "STAT_SpeedUp"
+      ],
+      "addStacksPerTrigger": 1,
+      "execute": [
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier",
+          "execute": [
+            {
+              "name": "Define Custom Variable with Modifier Values",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "valueType": "Layer",
+              "variableName": "MDF_Layer",
+              "modifierName": "<a class=\"gModGreen\" id=\"-1762822846\">JingYuan_BattleEvent_SpeedUp</a>",
+              "multiplier": 1
+            },
+            {
+              "name": "Stack Target Stat Value",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "statName": "&nbsp;<span class=\"descriptionNumberColor\">SPDFlat</span>&nbsp;",
+              "value": {
+                "operator": "Variables[0] (MDF_PropertyValue) || Variables[1] (MDF_Layer) || MUL || RETURN",
+                "displayLines": "(MDF_PropertyValue * MDF_Layer)",
+                "constants": [],
+                "variables": [
+                  "MDF_PropertyValue",
+                  "MDF_Layer"
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__1222558059\">JingYuan_BattleEvent</a>",
+      "stackType": "Prolong",
+      "lifeCyclePhaseAllowed": "ModifierPhase1End",
+      "execute": [
+        {
+          "eventTrigger": "Turn [Pre-action Phase]",
+          "execute": [
+            {
+              "name": "Inject Ability Use",
+              "abilityName": "JingYuan_Passive_Insert",
+              "abilitySource": {
+                "name": "Target Name",
+                "target": "{{Battle Event's Caster}}"
+              },
+              "abilityTarget": {
+                "name": "Target Name",
+                "target": "{{Enemy Team All}}"
+              },
+              "priorityTag": "CharacterAttackFromSelf",
+              "canHitNonTargets": true,
+              "abortFlags": [
+                "STAT_CTRL",
+                "DisableAction"
+              ],
+              "allowAbilityTriggers": false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-1804861609\">JingYuan_BattleEvent_UltraATKCount</a>[<span class=\"descriptionNumberColor\">Lightbringer</span>]",
+      "stackType": "ReplaceByCaster",
+      "description": "Lightning-Lord's Enhanced ATK count.",
+      "type": "Other",
+      "statusName": "Lightbringer",
+      "addStacksPerTrigger": 1
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__2081826421\">JingYuan_BattleEvent_ATKCount</a>[<span class=\"descriptionNumberColor\">Prana Extirpated</span>]",
+      "stackType": "ReplaceByCaster",
+      "description": "Lightning-Lord's Hits Per Action.",
+      "type": "Other",
+      "statusName": "Prana Extirpated",
+      "addStacksPerTrigger": 1
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__1141770667\">JingYuan_BattleEvent_ATKCountShowText</a>",
+      "stackType": "ReplaceByCaster"
+    }
+  ],
+  "references": []
+}
