63ccd7fb140bdb5eb267500386b4686a5c3c3d53
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:39:06 -0600

    30036

--- HonkaiSR/TheLibrary/AbilityConfigs/30036/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/30036/compositeAbilityObject.js
@@ -2,9 +2,399 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 30036,
   "trimCharacterName": 30036,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "30036_BattleEventAbility_Challenge_Xianzhou_06",
+    "30036_Modifiers"
+  ],
+  "abilityObject": {
+    "30036_BattleEventAbility_Challenge_Xianzhou_06": {
+      "fileName": "30036_BattleEventAbility_Challenge_Xianzhou_06",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "whenAdded": [
+        {
+          "name": "Add Events/Bonuses",
+          "to": {
+            "name": "Target Name",
+            "target": "{{Caster}}"
+          },
+          "modifier": "<a class=\"gModGreen\" id=\"334530977\">Modifier_BattleEventAbility_Challenge_Xianzhou_06</a>"
+        }
+      ],
+      "references": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__775144590\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub2</a>",
+          "execute": [
+            {
+              "eventTrigger": "Deal Damage Start [Owner]: Any",
+              "execute": [
+                {
+                  "name": "Adjust Target Stats",
+                  "modifiedValuesArray": [
+                    {
+                      "on": "Attacker",
+                      "statName": "&nbsp;<span class=\"descriptionNumberColor\">DamageAll</span>&nbsp;",
+                      "value": "MDF_PropertyValue"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__758366971\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub1</a>",
+          "execute": [
+            {
+              "eventTrigger": "Attack Start [Owner]",
+              "execute": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Skill Effect",
+                    "skillType": [
+                      "AoEAttack",
+                      "Blast"
+                    ],
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Modifier Holder}}"
+                    }
+                  },
+                  "passed": [
+                    {
+                      "name": "IF",
+                      "conditions": {
+                        "name": "Has Modifier",
+                        "target": {
+                          "name": "Target Name",
+                          "target": "{{Parameter Target}}"
+                        },
+                        "modifier": "<a class=\"gModGreen\" id=\"775144590\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub2</a>"
+                      },
+                      "failed": [
+                        {
+                          "name": "Adjust Target Stats",
+                          "modifiedValuesArray": [
+                            {
+                              "on": "Attacker",
+                              "statName": "&nbsp;<span class=\"descriptionNumberColor\">DamageAll</span>&nbsp;",
+                              "value": "PropertyValue"
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      "name": "Add Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Modifier Holder}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"775144590\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub2</a>",
+                      "valuePerStack": {
+                        "MDF_PropertyValue": {
+                          "operator": "Variables[0] (PropertyValue) || RETURN",
+                          "displayLines": "PropertyValue",
+                          "constants": [],
+                          "variables": [
+                            "PropertyValue"
+                          ]
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "eventTrigger": "Attack DMG End [Owner]",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"775144590\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub2</a>"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-434783800\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub</a>",
+          "execute": [
+            {
+              "eventTrigger": "Take Damage Start [Owner]: Any",
+              "execute": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "NOT",
+                    "condition": {
+                      "name": "Is Joint-Attack",
+                      "target": {
+                        "name": "Target Name",
+                        "target": "{{Modifier Holder}}"
+                      }
+                    }
+                  },
+                  "passed": [
+                    {
+                      "name": "Define Custom Variable with Team Count",
+                      "target": {
+                        "name": "Target Name",
+                        "target": "{{Enemy Team All}}"
+                      },
+                      "variableName": "DarkTeamCount",
+                      "conditions": {
+                        "name": "Compare: Variable",
+                        "target": {
+                          "name": "Target Name",
+                          "target": "{{Parameter Target}}"
+                        },
+                        "value1": "CurrentHP%",
+                        "compareType": ">",
+                        "value2": 0
+                      }
+                    },
+                    {
+                      "name": "Share DMG Taken",
+                      "target": {
+                        "name": "Target Name",
+                        "target": "{{Enemy Team All}}"
+                      },
+                      "selfSplit": {
+                        "operator": "Constants[0] (1) || Variables[0] (DarkTeamCount) || DIV || RETURN",
+                        "displayLines": "(1 / DarkTeamCount)",
+                        "constants": [
+                          1
+                        ],
+                        "variables": [
+                          "DarkTeamCount"
+                        ]
+                      },
+                      "targetSplit": {
+                        "operator": "Constants[0] (1) || Variables[0] (DarkTeamCount) || DIV || RETURN",
+                        "displayLines": "(1 / DarkTeamCount)",
+                        "constants": [
+                          1
+                        ],
+                        "variables": [
+                          "DarkTeamCount"
+                        ]
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__334530977\">Modifier_BattleEventAbility_Challenge_Xianzhou_06</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Add Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Player Team All}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"758366971\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub1</a>",
+                  "valuePerStack": {
+                    "PropertyValue": {
+                      "operator": "Variables[0] (#BattleEvent_P1_ADF) || RETURN",
+                      "displayLines": "#BattleEvent_P1_ADF",
+                      "constants": [],
+                      "variables": [
+                        "#BattleEvent_P1_ADF"
+                      ]
+                    }
+                  }
+                }
+              ]
+            },
+            {
+              "eventTrigger": "Entity Created [Anyone]",
+              "execute": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Is Part Of Team",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "team": "Enemy Team"
+                  },
+                  "passed": [
+                    {
+                      "name": "Add Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Parameter Target}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"-434783800\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub</a>"
+                    }
+                  ]
+                },
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Is Part Of Team",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "team": "Player Team"
+                  },
+                  "passed": [
+                    {
+                      "name": "Add Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Parameter Target}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"758366971\">Modifier_BattleEventAbility_Challenge_Xianzhou_06_Sub1</a>",
+                      "valuePerStack": {
+                        "PropertyValue": {
+                          "operator": "Variables[0] (#BattleEvent_P1_ADF) || RETURN",
+                          "displayLines": "#BattleEvent_P1_ADF",
+                          "constants": [],
+                          "variables": [
+                            "#BattleEvent_P1_ADF"
+                          ]
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "30036_Modifiers": {
+      "fileName": "30036_Modifiers",
+      "abilityType": "Char. Modifiers",
+      "energy": null,
+      "toughnessList": [
+        0,
+        0,
+        0
+      ],
+      "parse": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-1651606625\">MDF_RogueDLCAchievementListener</a>",
+          "execute": [
+            {
+              "eventTrigger": "Turn [Pre-action Phase]",
+              "execute": [
+                {
+                  "name": "Define Custom Variable",
+                  "variableName": "count",
+                  "value": {
+                    "operator": "Variables[0] (count) || Constants[0] (1) || ADD || RETURN",
+                    "displayLines": "(count + 1)",
+                    "constants": [
+                      1
+                    ],
+                    "variables": [
+                      "count"
+                    ]
+                  }
+                },
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Compare: Variable",
+                    "value1": "count",
+                    "compareType": "=",
+                    "value2": 10
+                  },
+                  "passed": [
+                    {
+                      "name": "Achievement",
+                      "relatedAchievements": [
+                        {
+                          "title": "All Watched Over By Machines of Loving Grace",
+                          "desc": "In Simulated Universe: Gold and Gears, endure #1[i] or more Resonance Extrapolation(s) in a single battle",
+                          "rarity": "Low",
+                          "type": "Hidden until Completion",
+                          "params": [
+                            10
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      "name": "Remove Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Modifier Holder}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"-1651606625\">MDF_RogueDLCAchievementListener</a>"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1216668349\">Challenge_Camera_AllTeam_Temp_2</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1216668349\">Challenge_Camera_AllTeam_Temp_2</a>"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1166335492\">Challenge_Camera_AllTeam_Temp_1</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1166335492\">Challenge_Camera_AllTeam_Temp_1</a>"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      "references": []
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
