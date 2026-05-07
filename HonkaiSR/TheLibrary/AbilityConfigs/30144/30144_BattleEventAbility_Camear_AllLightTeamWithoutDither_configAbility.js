267b7569f198f7c7b328fe00b6b37960319e0310
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 03:08:48 -0600

    30144

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30144/30144_BattleEventAbility_Camear_AllLightTeamWithoutDither_configAbility.js
@@ -0,0 +1,16 @@
+const configAbility = {
+  "fileName": "30144_BattleEventAbility_Camear_AllLightTeamWithoutDither",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "UI Display Event",
+      "popUpText": "Memory Turbulence"
+    }
+  ],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "Inherent Target"
+  }
+}
