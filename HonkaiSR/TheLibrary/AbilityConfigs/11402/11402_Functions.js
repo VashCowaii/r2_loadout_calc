7c2a2eb05eea0e3174726f276eaadc9eece0cfef
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 01:08:55 -0600

    11402

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/11402/11402_Functions.js
@@ -0,0 +1,116 @@
+const configAbility = {
+  "fileName": "11402_Functions",
+  "abilityType": "Char. Functions",
+  "energy": null,
+  "toughnessList": [
+    0,
+    0,
+    0
+  ],
+  "length": 1,
+  "parse": [
+    {
+      "name": "CharacterFunctions",
+      "functionName": "<a class=\"gTempYellow\" id=\"fun__-1952576234\">Aglaea_Eidolon6_SpeedValue</a>",
+      "parse": [
+        {
+          "name": "IF",
+          "conditions": {
+            "name": "Compare: Ability Value",
+            "target": {
+              "name": "Target Name",
+              "target": "{{Modifier Holder}}"
+            },
+            "value1": "&nbsp;<span class=\"descriptionNumberColor\">Speed</span>&nbsp;",
+            "compareType": ">",
+            "value2": 320
+          },
+          "passed": [
+            {
+              "name": "Define Modifier-Specific Variable",
+              "modifierName": "<a class=\"gModGreen\" id=\"1330366864\">Aglaea_Eidolon6_Effect</a>[<span class=\"descriptionNumberColor\">Fluctuate in the Tapestry of Fates</span>]",
+              "variableName": "MDF_PropertyValue2",
+              "value": {
+                "operator": "Variables[0] (UnusedUnderThisBase_12972) || RETURN",
+                "displayLines": "UnusedUnderThisBase_12972",
+                "constants": [],
+                "variables": [
+                  "UnusedUnderThisBase_12972"
+                ]
+              }
+            }
+          ],
+          "failed": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "Compare: Ability Value",
+                "target": {
+                  "name": "Target Name",
+                  "target": "{{Modifier Holder}}"
+                },
+                "value1": "&nbsp;<span class=\"descriptionNumberColor\">Speed</span>&nbsp;",
+                "compareType": ">",
+                "value2": 240
+              },
+              "passed": [
+                {
+                  "name": "Define Modifier-Specific Variable",
+                  "modifierName": "<a class=\"gModGreen\" id=\"1330366864\">Aglaea_Eidolon6_Effect</a>[<span class=\"descriptionNumberColor\">Fluctuate in the Tapestry of Fates</span>]",
+                  "variableName": "MDF_PropertyValue2",
+                  "value": {
+                    "operator": "Variables[0] (UnusedUnderThisBase_12986) || RETURN",
+                    "displayLines": "UnusedUnderThisBase_12986",
+                    "constants": [],
+                    "variables": [
+                      "UnusedUnderThisBase_12986"
+                    ]
+                  }
+                }
+              ],
+              "failed": [
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Compare: Ability Value",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Modifier Holder}}"
+                    },
+                    "value1": "&nbsp;<span class=\"descriptionNumberColor\">Speed</span>&nbsp;",
+                    "compareType": ">",
+                    "value2": 160
+                  },
+                  "passed": [
+                    {
+                      "name": "Define Modifier-Specific Variable",
+                      "modifierName": "<a class=\"gModGreen\" id=\"1330366864\">Aglaea_Eidolon6_Effect</a>[<span class=\"descriptionNumberColor\">Fluctuate in the Tapestry of Fates</span>]",
+                      "variableName": "MDF_PropertyValue2",
+                      "value": {
+                        "operator": "Variables[0] (UnusedUnderThisBase_12970) || RETURN",
+                        "displayLines": "UnusedUnderThisBase_12970",
+                        "constants": [],
+                        "variables": [
+                          "UnusedUnderThisBase_12970"
+                        ]
+                      }
+                    }
+                  ],
+                  "failed": [
+                    {
+                      "name": "Define Modifier-Specific Variable",
+                      "modifierName": "<a class=\"gModGreen\" id=\"1330366864\">Aglaea_Eidolon6_Effect</a>[<span class=\"descriptionNumberColor\">Fluctuate in the Tapestry of Fates</span>]",
+                      "variableName": "MDF_PropertyValue2",
+                      "value": 0
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "references": []
+}
