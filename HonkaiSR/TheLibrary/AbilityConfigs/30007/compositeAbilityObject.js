c981703aad1acb90e27020ffbdc61e4e26c6f35f
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:02:56 -0600

    30007

--- HonkaiSR/TheLibrary/AbilityConfigs/30007/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/30007/compositeAbilityObject.js
@@ -2,9 +2,223 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 30007,
   "trimCharacterName": 30007,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "30007_BattleEventAbility_Challenge_07",
+    "30007_Modifiers"
+  ],
+  "abilityObject": {
+    "30007_BattleEventAbility_Challenge_07": {
+      "fileName": "30007_BattleEventAbility_Challenge_07",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "whenAdded": [
+        {
+          "name": "Add Events/Bonuses",
+          "to": {
+            "name": "Target Name",
+            "target": "{{Caster}}"
+          },
+          "modifier": "<a class=\"gModGreen\" id=\"1734301025\">Modifier_BattleEventAbility_Challenge_07</a>"
+        }
+      ],
+      "references": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-235869909\">MLevel_BattleEventAbility_Challenge_07</a>[<span class=\"descriptionNumberColor\">Quantum Vulnerability</span>]",
+          "stackType": "ReplaceByCaster",
+          "description": "Quantum DMG taken +<span class=\"descriptionNumberColor\">#BattleEvent_P2_ADF</span>.",
+          "type": "Debuff",
+          "effectName": "Quantum Vulnerability",
+          "statusName": "Quantum Vulnerability",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Stack Target Stat Value",
+                  "target": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "statName": "&nbsp;<span class=\"descriptionNumberColor\">QuantumVulnerability</span>&nbsp;",
+                  "value": {
+                    "operator": "Variables[0] (#BattleEvent_P2_ADF) || RETURN",
+                    "displayLines": "#BattleEvent_P2_ADF",
+                    "constants": [],
+                    "variables": [
+                      "#BattleEvent_P2_ADF"
+                    ]
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1734301025\">Modifier_BattleEventAbility_Challenge_07</a>",
+          "execute": [
+            {
+              "eventTrigger": "Turn [Pre-action Phase]",
+              "execute": [
+                {
+                  "name": "Find New Target",
+                  "from": {
+                    "name": "Target Name",
+                    "target": "{{Enemy Team All}}"
+                  },
+                  "searchRandom": true,
+                  "maxTargets": 1,
+                  "ifTargetFound": [
+                    {
+                      "name": "Trigger Ability",
+                      "from": {
+                        "name": "Target Name",
+                        "target": "{{Caster}}"
+                      },
+                      "inherentTarget": {
+                        "name": "Target Name",
+                        "target": "{{Parameter Target}}"
+                      },
+                      "ability": "BattleEventAbility_Camear_SelectDarkTeamEntity",
+                      "isTrigger": true
+                    },
+                    {
+                      "name": "Add Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Parameter Target}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"-235869909\">MLevel_BattleEventAbility_Challenge_07</a>[<span class=\"descriptionNumberColor\">Quantum Vulnerability</span>]",
+                      "duration": {
+                        "operator": "Variables[0] (#BattleEvent_P3_ADF) || RETURN",
+                        "displayLines": "#BattleEvent_P3_ADF",
+                        "constants": [],
+                        "variables": [
+                          "#BattleEvent_P3_ADF"
+                        ]
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "30007_Modifiers": {
+      "fileName": "30007_Modifiers",
+      "abilityType": "Char. Modifiers",
+      "energy": null,
+      "toughnessList": [
+        0,
+        0,
+        0
+      ],
+      "parse": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-1651606625\">MDF_RogueDLCAchievementListener</a>",
+          "execute": [
+            {
+              "eventTrigger": "Turn [Pre-action Phase]",
+              "execute": [
+                {
+                  "name": "Define Custom Variable",
+                  "variableName": "count",
+                  "value": {
+                    "operator": "Variables[0] (count) || Constants[0] (1) || ADD || RETURN",
+                    "displayLines": "(count + 1)",
+                    "constants": [
+                      1
+                    ],
+                    "variables": [
+                      "count"
+                    ]
+                  }
+                },
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Compare: Variable",
+                    "value1": "count",
+                    "compareType": "=",
+                    "value2": 10
+                  },
+                  "passed": [
+                    {
+                      "name": "Achievement",
+                      "relatedAchievements": [
+                        {
+                          "title": "All Watched Over By Machines of Loving Grace",
+                          "desc": "In Simulated Universe: Gold and Gears, endure #1[i] or more Resonance Extrapolation(s) in a single battle",
+                          "rarity": "Low",
+                          "type": "Hidden until Completion",
+                          "params": [
+                            10
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      "name": "Remove Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Modifier Holder}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"-1651606625\">MDF_RogueDLCAchievementListener</a>"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1216668349\">Challenge_Camera_AllTeam_Temp_2</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1216668349\">Challenge_Camera_AllTeam_Temp_2</a>"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1166335492\">Challenge_Camera_AllTeam_Temp_1</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1166335492\">Challenge_Camera_AllTeam_Temp_1</a>"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      "references": []
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
