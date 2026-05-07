d42f60d20dd25005ce095c5b65ec7bb375e87c2a
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:46:59 -0600

    30114

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30114/30114_Standard_Servant_Forcekill_configAbility.js
@@ -0,0 +1,20 @@
+const configAbility = {
+  "fileName": "30114_Standard_Servant_Forcekill",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Force Entity Death",
+      "target": {
+        "name": "Target Name",
+        "target": "{{Ability Target(ST)}}"
+      },
+      "abortInsertedAbilities": true
+    }
+  ],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  }
+}
