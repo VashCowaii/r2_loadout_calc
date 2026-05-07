776254bdd417e36ae2f18d605277f84cbf5d6b68
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 07:57:05 -0600

    60040

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/60040/60040_MissionBattleEvent60039_Player_Ability03_Part01_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "60040_MissionBattleEvent60039_Player_Ability03_Part01",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  }
+}
