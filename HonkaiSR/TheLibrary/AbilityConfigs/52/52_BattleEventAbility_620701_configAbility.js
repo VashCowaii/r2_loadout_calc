e0ea5094756e33aafa71031278ccd69c8b27f1f0
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 07:38:22 -0600

    52

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/52/52_BattleEventAbility_620701_configAbility.js
@@ -0,0 +1,9 @@
+const configAbility = {
+  "fileName": "52_BattleEventAbility_620701",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "whenAdded": [],
+  "references": []
+}
--- HonkaiSR/TheLibrary/AbilityConfigs/52/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/52/compositeAbilityObject.js
@@ -2,9 +2,32 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 52,
   "trimCharacterName": 52,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "52_BattleEventAbility_620701_Camera",
+    "52_BattleEventAbility_620701"
+  ],
+  "abilityObject": {
+    "52_BattleEventAbility_620701_Camera": {
+      "fileName": "52_BattleEventAbility_620701_Camera",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "references": [],
+      "targetObjectData": {
+        "primaryTarget": "{{Caster}}"
+      }
+    },
+    "52_BattleEventAbility_620701": {
+      "fileName": "52_BattleEventAbility_620701",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "whenAdded": [],
+      "references": []
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
