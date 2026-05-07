0315cc6a98470a5d19af555f8f8b5ff932e95f2f
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:00:40 -0600

    3

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/3/3_BattleEventAbility_ChallengeTurnLimit_configAbility.js
@@ -0,0 +1,18 @@
+const configAbility = {
+  "fileName": "3_BattleEventAbility_ChallengeTurnLimit",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "whenAdded": [
+    {
+      "name": "Add Events/Bonuses",
+      "to": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "modifier": "<a class=\"gModGreen\" id=\"-272210701\">Standard_ChallengeTurnLimit</a>"
+    }
+  ],
+  "references": []
+}
