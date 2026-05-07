6d7f345ee12956dc135b6451fe67aa955cefd312
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 03:18:02 -0600

    42

--- HonkaiSR/TheLibrary/AbilityConfigs/42/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/42/compositeAbilityObject.js
@@ -2,9 +2,22 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 42,
   "trimCharacterName": 42,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "42_BattleEventAbility_620101_Camera"
+  ],
+  "abilityObject": {
+    "42_BattleEventAbility_620101_Camera": {
+      "fileName": "42_BattleEventAbility_620101_Camera",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "references": [],
+      "targetObjectData": {
+        "primaryTarget": "{{Caster}}"
+      }
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
