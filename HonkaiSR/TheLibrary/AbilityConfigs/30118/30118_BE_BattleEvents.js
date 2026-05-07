b0edb594adcf28633bbd120a3206f48a714bf0ea
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:49:22 -0600

    30118

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30118/30118_BE_BattleEvents.js
@@ -0,0 +1,26 @@
+const configAbility = {
+  "fileName": "30118_BE_BattleEvents",
+  "abilityType": "Char. B.Events",
+  "energy": null,
+  "toughnessList": [
+    0,
+    0,
+    0
+  ],
+  "parse": [
+    {
+      "name": "Battle Event Construction",
+      "ID": 31003,
+      "team": "Neutral Team",
+      "eventType": "Assist",
+      "abilityList": null,
+      "overridesArray": [
+        {
+          "statName": "&nbsp;<span class=\"descriptionNumberColor\">ATKBase</span>&nbsp;",
+          "value": 100
+        }
+      ]
+    }
+  ],
+  "references": []
+}
