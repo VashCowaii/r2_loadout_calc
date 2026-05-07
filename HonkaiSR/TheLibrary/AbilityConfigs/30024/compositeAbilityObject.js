02f5b5c4a2427905653f1d9a1605db6a29aadca1
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:09:12 -0600

    30024

--- HonkaiSR/TheLibrary/AbilityConfigs/30024/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/30024/compositeAbilityObject.js
@@ -2,9 +2,209 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 30024,
   "trimCharacterName": 30024,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "30024_BattleEventAbility_Challenge_New_07",
+    "30024_Modifiers"
+  ],
+  "abilityObject": {
+    "30024_BattleEventAbility_Challenge_New_07": {
+      "fileName": "30024_BattleEventAbility_Challenge_New_07",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "whenAdded": [
+        {
+          "name": "Add Events/Bonuses",
+          "to": {
+            "name": "Target Name",
+            "target": "{{Caster}}"
+          },
+          "modifier": "<a class=\"gModGreen\" id=\"856090006\">Modifier_BattleEventAbility_Challenge_New_07</a>"
+        }
+      ],
+      "references": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__856090006\">Modifier_BattleEventAbility_Challenge_New_07</a>",
+          "execute": [
+            {
+              "eventTrigger": "Turn [Pre-action Phase]",
+              "execute": [
+                {
+                  "name": "Trigger Ability",
+                  "from": {
+                    "name": "Target Name",
+                    "target": "{{Caster}}"
+                  },
+                  "inherentTarget": {
+                    "name": "Target Name",
+                    "target": "{{Player Team Entity}}"
+                  },
+                  "ability": "BattleEventAbility_Camear_AllLightTeam",
+                  "isTrigger": true
+                },
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Player Team All}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"-1797420881\">Standard_Protect_Shield</a>[<span class=\"descriptionNumberColor\">Shield</span>]"
+                },
+                {
+                  "name": "Find New Target",
+                  "from": {
+                    "name": "Target Name",
+                    "target": "{{Player Team All}}"
+                  },
+                  "ifTargetFound": [
+                    {
+                      "name": "Define Custom Variable with Stat",
+                      "target": {
+                        "name": "Target Name",
+                        "target": "{{Parameter Target}}"
+                      },
+                      "variableName": "EntityMaxHP",
+                      "value": "&nbsp;<span class=\"descriptionNumberColor\">HPMax</span>&nbsp;"
+                    },
+                    {
+                      "name": "Add Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Parameter Target}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"-1797420881\">Standard_Protect_Shield</a>[<span class=\"descriptionNumberColor\">Shield</span>]",
+                      "valuePerStack": {
+                        "MDF_ShowValue": {
+                          "operator": "Variables[0] (EntityMaxHP) || Variables[1] (#BattleEvent_P1_ADF) || MUL || RETURN",
+                          "displayLines": "(EntityMaxHP * #BattleEvent_P1_ADF)",
+                          "constants": [],
+                          "variables": [
+                            "EntityMaxHP",
+                            "#BattleEvent_P1_ADF"
+                          ]
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "30024_Modifiers": {
+      "fileName": "30024_Modifiers",
+      "abilityType": "Char. Modifiers",
+      "energy": null,
+      "toughnessList": [
+        0,
+        0,
+        0
+      ],
+      "parse": [
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__-1651606625\">MDF_RogueDLCAchievementListener</a>",
+          "execute": [
+            {
+              "eventTrigger": "Turn [Pre-action Phase]",
+              "execute": [
+                {
+                  "name": "Define Custom Variable",
+                  "variableName": "count",
+                  "value": {
+                    "operator": "Variables[0] (count) || Constants[0] (1) || ADD || RETURN",
+                    "displayLines": "(count + 1)",
+                    "constants": [
+                      1
+                    ],
+                    "variables": [
+                      "count"
+                    ]
+                  }
+                },
+                {
+                  "name": "IF",
+                  "conditions": {
+                    "name": "Compare: Variable",
+                    "value1": "count",
+                    "compareType": "=",
+                    "value2": 10
+                  },
+                  "passed": [
+                    {
+                      "name": "Achievement",
+                      "relatedAchievements": [
+                        {
+                          "title": "All Watched Over By Machines of Loving Grace",
+                          "desc": "In Simulated Universe: Gold and Gears, endure #1[i] or more Resonance Extrapolation(s) in a single battle",
+                          "rarity": "Low",
+                          "type": "Hidden until Completion",
+                          "params": [
+                            10
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      "name": "Remove Events/Bonuses",
+                      "to": {
+                        "name": "Target Name",
+                        "target": "{{Modifier Holder}}"
+                      },
+                      "modifier": "<a class=\"gModGreen\" id=\"-1651606625\">MDF_RogueDLCAchievementListener</a>"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1216668349\">Challenge_Camera_AllTeam_Temp_2</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1216668349\">Challenge_Camera_AllTeam_Temp_2</a>"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Modifier Construction",
+          "for": "<a class=\"gModGreen\" id=\"mod__1166335492\">Challenge_Camera_AllTeam_Temp_1</a>",
+          "execute": [
+            {
+              "eventTrigger": "When Stacking/Receiving Modifier",
+              "execute": [
+                {
+                  "name": "Remove Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Modifier Holder}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"1166335492\">Challenge_Camera_AllTeam_Temp_1</a>"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      "references": []
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
