5eb5ccd12e37dcdeaff0833e9750c5b72cf19e6b
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 02:35:42 -0600

    30031

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30031/30031_BattleEventAbility_Challenge_Xianzhou_01_Camera_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "30031_BattleEventAbility_Challenge_Xianzhou_01_Camera",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  }
+}
