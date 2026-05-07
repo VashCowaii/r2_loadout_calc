4b1cef487648e510409dbd8784363bac714f0852
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 03:13:10 -0600

    30504

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30504/30504_BattleEventAbility_ChallengePeakBattle_HardBossScreenEffect_configAbility.js
@@ -0,0 +1,9 @@
+const configAbility = {
+  "fileName": "30504_BattleEventAbility_ChallengePeakBattle_HardBossScreenEffect",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "whenAdded": [],
+  "references": []
+}
