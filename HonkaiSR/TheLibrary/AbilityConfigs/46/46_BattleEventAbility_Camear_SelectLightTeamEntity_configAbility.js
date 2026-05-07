6c2af5a9c95475e095f8f1781d9db9f14baed966
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 03:29:19 -0600

    46

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/46/46_BattleEventAbility_Camear_SelectLightTeamEntity_configAbility.js
@@ -0,0 +1,16 @@
+const configAbility = {
+  "fileName": "46_BattleEventAbility_Camear_SelectLightTeamEntity",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "UI Display Event",
+      "popUpText": "Memory Turbulence"
+    }
+  ],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "Inherent Target"
+  }
+}
