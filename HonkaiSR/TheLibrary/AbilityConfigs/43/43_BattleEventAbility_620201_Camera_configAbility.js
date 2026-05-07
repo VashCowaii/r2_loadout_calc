2935af17675c743f0e39dfc8803a9f2f9fee4509
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-07 03:18:14 -0600

    43

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/43/43_BattleEventAbility_620201_Camera_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "43_BattleEventAbility_620201_Camera",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  }
+}
--- HonkaiSR/TheLibrary/AbilityConfigs/43/compositeAbilityObject.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/43/compositeAbilityObject.js
@@ -2,9 +2,22 @@ const entityPageType = "misc"
 const compositeAbilityObject = {
   "fullCharacterName": 43,
   "trimCharacterName": 43,
-  "abilityList": [],
-  "abilityObject": {},
+  "abilityList": [
+    "43_BattleEventAbility_620201_Camera"
+  ],
+  "abilityObject": {
+    "43_BattleEventAbility_620201_Camera": {
+      "fileName": "43_BattleEventAbility_620201_Camera",
+      "abilityType": null,
+      "energy": null,
+      "toughnessList": null,
+      "parse": [],
+      "references": [],
+      "targetObjectData": {
+        "primaryTarget": "{{Caster}}"
+      }
+    }
+  },
   "enemyData": {},
-  "isBattleEvent": true,
-  "noReader": true
+  "isBattleEvent": true
 }
