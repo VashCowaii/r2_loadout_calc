ce934f4b43637222b3c2ca0cd30ce5852196dffc
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-08 11:48:27 -0600

    overworld TBD

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/TrailblazerDestruction/TrailblazerDestruction_LocalPlayer_PlayerBoy_TechniqueUsage_configAbility.js
@@ -0,0 +1,18 @@
+const configAbility = {
+  "fileName": "TrailblazerDestruction_LocalPlayer_PlayerBoy_TechniqueUsage",
+  "skillTrigger": "MazeSkill",
+  "abilityType": "Basic ATK",
+  "toughnessList": null,
+  "parse": [
+    "Deleted bullshit",
+    "Overworld HP adjustment(which we don't care about)",
+    "Submit Technique Use"
+  ],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "{{Caster}}"
+  }
+}
