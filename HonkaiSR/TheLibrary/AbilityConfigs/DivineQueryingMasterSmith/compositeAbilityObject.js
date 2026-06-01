c317fcb6860a1b962fe03abebddf8d341b294f88
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-06-01 01:49:07 -0600

    add new relic base pages

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/DivineQueryingMasterSmith/compositeAbilityObject.js
@@ -0,0 +1,16 @@
+const entityPageType = "relic"
+const compositeAbilityObject = {
+  "fullCharacterName": "Divine-Querying Master Smith",
+  "trimCharacterName": "DivineQueryingMasterSmith",
+  "abilityList": [],
+  "abilityObject": {},
+  "fixedStats": {
+    "2": {
+      "HP%": 0.12
+    },
+    "4": {}
+  },
+  "isLightcone": true,
+  "isRelic": true,
+  "noReader": true
+}
