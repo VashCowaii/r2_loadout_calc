bd6988319a22d040948f37567d6ed96c1a404164
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-05-31 19:43:04 -0600

    add new enemy base pages

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/4064012/dEnemyData2.js
@@ -0,0 +1,749 @@
+let enemyData = {
+  "name": "Arbiter of the Lost Abyss",
+  "image": 4064012,
+  "bar": 2,
+  "bodyType": "LargeMonster",
+  "rank": "LittleBoss",
+  "stArray": [
+    18,
+    210,
+    27900,
+    144,
+    null,
+    0.2,
+    0.3,
+    1,
+    360,
+    null,
+    8
+  ],
+  "options": {
+    "4064012": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Physical",
+        "Fire",
+        "Thunder",
+        "Imaginary"
+      ],
+      "res": {
+        "Ice": 0.2,
+        "Wind": 0.2,
+        "Quantum": 0.2
+      },
+      "resMod": {
+        "STAT_CTRL": 0.75
+      },
+      "abilities": [
+        2492,
+        2493,
+        2494,
+        2495,
+        2496,
+        2497,
+        2498,
+        2499,
+        2500,
+        2502,
+        2503,
+        2504,
+        2505,
+        2506,
+        2507,
+        2501
+      ],
+      "summons": [
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201002,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201007,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Canine",
+          "over": 406202002,
+          "base": 4062020
+        },
+        {
+          "name": "Ichor Memosprite: Winged Serpent",
+          "over": 406301003,
+          "base": 4063010
+        }
+      ],
+      "itNeverEnds": {
+        "HardLevel": 1,
+        "SummonID": 406201002
+      }
+    },
+    "406401201": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Physical",
+        "Fire",
+        "Thunder",
+        "Imaginary"
+      ],
+      "res": {
+        "Ice": 0.2,
+        "Wind": 0.2,
+        "Quantum": 0.2
+      },
+      "resMod": {
+        "STAT_CTRL": 0.75
+      },
+      "abilities": [
+        2492,
+        2493,
+        2494,
+        2495,
+        2496,
+        2497,
+        2498,
+        2499,
+        2500,
+        2502,
+        2503,
+        2504,
+        2505,
+        2506,
+        2507,
+        2501
+      ],
+      "summons": [
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201002,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201007,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Canine",
+          "over": 406202002,
+          "base": 4062020
+        },
+        {
+          "name": "Ichor Memosprite: Winged Serpent",
+          "over": 406301003,
+          "base": 4063010
+        }
+      ],
+      "overrideParams": {
+        "2492": [
+          [
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2493": [
+          [
+            0.25,
+            -1,
+            -1,
+            0.75
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2495": [
+          [
+            5.4
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2496": [
+          [
+            3.2
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2498": [
+          [
+            11,
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2500": [
+          [
+            0.1
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2501": [
+          [
+            30
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2505": [
+          [
+            3.5
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ]
+      },
+      "itNeverEnds": {
+        "HardLevel": 1,
+        "SummonID": 406201002
+      }
+    },
+    "406401202": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Physical",
+        "Fire",
+        "Thunder",
+        "Imaginary"
+      ],
+      "res": {
+        "Ice": 0.2,
+        "Wind": 0.2,
+        "Quantum": 0.2
+      },
+      "resMod": {
+        "STAT_CTRL": 0.75
+      },
+      "abilities": [
+        2492,
+        2493,
+        2494,
+        2495,
+        2496,
+        2497,
+        2498,
+        2499,
+        2500,
+        2502,
+        2503,
+        2504,
+        2505,
+        2506,
+        2507,
+        2501
+      ],
+      "summons": [
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201003,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201008,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Canine",
+          "over": 406202003,
+          "base": 4062020
+        },
+        {
+          "name": "Ichor Memosprite: Winged Serpent",
+          "over": 406301004,
+          "base": 4063010
+        }
+      ],
+      "overrideParams": {
+        "2492": [
+          [
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2493": [
+          [
+            0.25,
+            -1,
+            -1,
+            0.75
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2495": [
+          [
+            5.4
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2496": [
+          [
+            3.2
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2498": [
+          [
+            11,
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2500": [
+          [
+            0.1
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2505": [
+          [
+            3.5
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ]
+      },
+      "itNeverEnds": {
+        "HardLevel": 2,
+        "SummonID": 406201003
+      }
+    },
+    "406401203": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1.1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Physical",
+        "Fire",
+        "Thunder",
+        "Imaginary"
+      ],
+      "res": {
+        "Ice": 0.2,
+        "Wind": 0.2,
+        "Quantum": 0.2
+      },
+      "resMod": {
+        "STAT_CTRL": 0.75
+      },
+      "abilities": [
+        2492,
+        2493,
+        2494,
+        2495,
+        2496,
+        2497,
+        2498,
+        2499,
+        2500,
+        2502,
+        2503,
+        2504,
+        2505,
+        2506,
+        2507,
+        2501
+      ],
+      "summons": [
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201004,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201009,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Canine",
+          "over": 406202004,
+          "base": 4062020
+        },
+        {
+          "name": "Ichor Memosprite: Winged Serpent",
+          "over": 406301005,
+          "base": 4063010
+        }
+      ],
+      "overrideParams": {
+        "2492": [
+          [
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2493": [
+          [
+            0.25,
+            -1,
+            -1,
+            0.75
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2495": [
+          [
+            5.4
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2496": [
+          [
+            3.2
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2498": [
+          [
+            11,
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2500": [
+          [
+            0.1
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2505": [
+          [
+            3.5
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ]
+      },
+      "itNeverEnds": {
+        "HardLevel": 3,
+        "SummonID": 406201004
+      }
+    },
+    "406401204": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1.1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Physical",
+        "Fire",
+        "Thunder",
+        "Imaginary"
+      ],
+      "res": {
+        "Ice": 0.2,
+        "Wind": 0.2,
+        "Quantum": 0.2
+      },
+      "resMod": {
+        "STAT_CTRL": 0.75
+      },
+      "abilities": [
+        2492,
+        2493,
+        2494,
+        2495,
+        2496,
+        2497,
+        2498,
+        2499,
+        2500,
+        2502,
+        2503,
+        2504,
+        2505,
+        2506,
+        2507,
+        2501
+      ],
+      "summons": [
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201005,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Pheasant",
+          "over": 406201010,
+          "base": 4062010
+        },
+        {
+          "name": "Ichor Memosprite: Canine",
+          "over": 406202005,
+          "base": 4062020
+        },
+        {
+          "name": "Ichor Memosprite: Winged Serpent",
+          "over": 406301006,
+          "base": 4063010
+        }
+      ],
+      "overrideParams": {
+        "2492": [
+          [
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2493": [
+          [
+            0.25,
+            -1,
+            -1,
+            0.75
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2495": [
+          [
+            5.4
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2496": [
+          [
+            3.2
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2498": [
+          [
+            11,
+            3.6
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2500": [
+          [
+            0.1
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ],
+        "2505": [
+          [
+            3.5
+          ],
+          null,
+          null,
+          null,
+          null,
+          null,
+          "-",
+          "-"
+        ]
+      },
+      "itNeverEnds": {
+        "HardLevel": 4,
+        "SummonID": 406201005
+      }
+    }
+  }
+}
---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5014030/dEnemyData2.js
@@ -0,0 +1,64 @@
+let enemyData = {
+  "name": "Murata Graphia, Founding Artist",
+  "image": 5014030,
+  "bar": 2,
+  "bodyType": "LargeMonster",
+  "rank": "LittleBoss",
+  "stArray": [
+    18,
+    210,
+    3022.5,
+    120,
+    null,
+    0.2,
+    0.3,
+    1,
+    1080,
+    null,
+    1
+  ],
+  "options": {
+    "5014030": {
+      "name": -1,
+      "scaleElite": 1,
+      "stArray": [
+        1,
+        1,
+        1,
+        1,
+        null,
+        1,
+        null
+      ],
+      "weak": [
+        "Fire",
+        "Thunder",
+        "Quantum"
+      ],
+      "res": {
+        "Physical": 0.4,
+        "Ice": 0.4,
+        "Wind": 0.4,
+        "Imaginary": 0.4
+      },
+      "resMod": {
+        "STAT_CTRL": 0.5
+      },
+      "abilities": [
+        2681,
+        2682,
+        2683,
+        2684,
+        2685,
+        2686
+      ],
+      "summons": [
+        {
+          "name": "God-Devourer Offspring",
+          "over": 502302001,
+          "base": 5023020
+        }
+      ]
+    }
+  }
+}
