5697ed6dc060d466669d89a8d132334fe93d85e0
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-06-01 14:22:11 -0600

    master smith reader

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/DivineQueryingMasterSmith/DivineQueryingMasterSmith_Ability51321_configAbility.js
@@ -0,0 +1,169 @@
+const configAbility = {
+  "fileName": "DivineQueryingMasterSmith_Ability51321",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Add Events/Bonuses",
+      "to": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "modifier": "<a class=\"gModGreen\" id=\"96407710\">Relic_132_Main</a>"
+    }
+  ],
+  "references": [
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__876387843\">Relic_132_Sub</a>[<span class=\"descriptionNumberColor\">Comburent</span>]",
+      "stackType": "Replace",
+      "description": "DMG dealt increases by <span class=\"descriptionNumberColor\">#SkillRelic_132_4_P3_DamageAddedRatio</span>.",
+      "type": "Buff",
+      "statusName": "Comburent",
+      "execute": [
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier",
+          "execute": [
+            {
+              "name": "Stack Target Stat Value",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "statName": "&nbsp;<span class=\"descriptionNumberColor\">DamageAll</span>&nbsp;",
+              "value": {
+                "operator": "Variables[0] (0.15) || RETURN",
+                "displayLines": "0.15",
+                "constants": [],
+                "variables": [
+                  0.15
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__96407710\">Relic_132_Main</a>",
+      "execute": [
+        {
+          "eventTrigger": "Deal Damage Start [Owner]: Any",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "Has Flag",
+                "target": {
+                  "name": "Target Name",
+                  "target": "{{Parameter Target}}"
+                },
+                "flagName": [
+                  "STAT_DefenceDown"
+                ]
+              },
+              "passed": [
+                {
+                  "name": "Adjust Target Stats",
+                  "modifiedValuesArray": [
+                    {
+                      "on": "Attacker",
+                      "statName": "&nbsp;<span class=\"descriptionNumberColor\">CritDamageSUM</span>&nbsp;",
+                      "value": "0.28"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "eventTrigger": "Attack Action Completed [Owner]",
+          "execute": [
+            {
+              "name": "Define Custom Variable",
+              "variableName": "Attack_Flag",
+              "value": 0
+            }
+          ]
+        },
+        {
+          "eventTrigger": "Apply Modifier [From Owner] (Success)",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "AND",
+                "conditionList": [
+                  {
+                    "name": "Is Part Of Team",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "team": "Enemy Team"
+                  },
+                  {
+                    "name": "Modifier Has Flag",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "flagName": "STAT_DefenceDown"
+                  },
+                  {
+                    "name": "Compare: Variable",
+                    "value1": "Attack_Flag",
+                    "compareType": "=",
+                    "value2": 0
+                  }
+                ]
+              },
+              "passed": [
+                {
+                  "name": "Define Custom Variable",
+                  "variableName": "Attack_Flag",
+                  "value": 1
+                },
+                {
+                  "name": "Add Events/Bonuses",
+                  "to": {
+                    "name": "Target Name",
+                    "target": "{{Player Team All}}"
+                  },
+                  "modifier": "<a class=\"gModGreen\" id=\"876387843\">Relic_132_Sub</a>[<span class=\"descriptionNumberColor\">Comburent</span>]",
+                  "duration": {
+                    "operator": "Variables[0] (2) || RETURN",
+                    "displayLines": "2",
+                    "constants": [],
+                    "variables": [
+                      2
+                    ]
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "isLightcone": true,
+  "isRelic": true,
+  "desc": [
+    "Increases Max HP by 12%.",
+    "Increases the wearer's CRIT DMG dealt to enemy targets in the DEF reduction state by 28%. After the wearer inflicts the DEF reduction state on an enemy target, all allies gain \"Comburent\" for 2 turn(s). This effect cannot be stacked. The DMG dealt by ally targets with \"Comburent\" increases by 15%. This effect can be triggered again after the wearer uses an attack."
+  ],
+  "params": [
+    [
+      0.12
+    ],
+    [
+      0.28,
+      2,
+      0.15
+    ]
+  ]
+}
