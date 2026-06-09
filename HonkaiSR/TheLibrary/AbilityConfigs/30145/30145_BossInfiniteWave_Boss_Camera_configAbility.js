76ae81718e26057f8f85b2be82e4fdc55b1e99aa
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-06-09 15:01:28 -0600

    30141/2/5/6

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/30145/30145_BossInfiniteWave_Boss_Camera_configAbility.js
@@ -0,0 +1,11 @@
+const configAbility = {
+  "fileName": "30145_BossInfiniteWave_Boss_Camera",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "references": [],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  }
+}
