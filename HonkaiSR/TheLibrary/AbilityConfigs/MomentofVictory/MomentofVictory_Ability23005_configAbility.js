c57c0dfd205f4c58caf895fc7db396373cd28e76
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-06-17 02:53:40 -0600

    update naming

--- HonkaiSR/TheLibrary/AbilityConfigs/MomentofVictory/MomentofVictory_Ability23005_configAbility.js
+++ HonkaiSR/TheLibrary/AbilityConfigs/MomentofVictory/MomentofVictory_Ability23005_configAbility.js
@@ -96,7 +96,7 @@ const configAbility = {
           ]
         },
         {
-          "eventTrigger": "Being Attacked End [Owner]",
+          "eventTrigger": "Being Attacked [Owner]: End",
           "execute": [
             {
               "name": "Add Events/Bonuses",
