77f4f7f7c5e551df1f8ce6b69f4bf7edf8721f0f
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-06-22 06:40:05 -0600

    update naming

--- HonkaiSR/TheLibrary/EnemyConfigs/dMegaMappingPathingExpand.js
+++ HonkaiSR/TheLibrary/EnemyConfigs/dMegaMappingPathingExpand.js
@@ -1,8 +1,10 @@
 let megaMappingPathing = {
+  "104715053": "AbilityGlobalP",
   "545784093": "AbilityConfigsGModADV",
   "884306140": "AbilityConfigsGMod",
   "1265477859": "AbilityConfigsBE",
   "1331461059": "EnemyConfigs",
+  "1362473331": "AbilityGlobalP",
   "1867055227": "AbilityConfigsGen",
   "2007735874": "AbilityConfigs",
   "-591498775": "AbilityConfigsTemp",
