b7d7ab33f195597a802a49d15fc40faec68fff52
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-07-14 21:01:28 -0600

    warning

--- HonkaiSR/TheLibrary/vashWarning.js
+++ HonkaiSR/TheLibrary/vashWarning.js
@@ -1,5 +1,5 @@
 // let currentWarning = "NOTE:<br>The missing hp amounts AFTER phase 1, on 4.0 enemies, should be resolved soonTM.";
-let currentWarning = ``;
+let currentWarning = `4.4 data is in progress. Some data will be incomplete for a few days.`;
 
 if (currentWarning) {readSelection("vashCustomWarningNote").innerHTML = currentWarning;}
 else {readSelection("vashCustomWarningNote").style.display = "none";}
