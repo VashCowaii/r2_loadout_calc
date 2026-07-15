d60281b97d58619d48d351591b5538e6a497759b
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-07-14 19:20:15 -0600

    weakness tracker

--- HonkaiSR/TheLibrary/EnemyConfigs/weaknessTracking.js
+++ HonkaiSR/TheLibrary/EnemyConfigs/weaknessTracking.js
@@ -1,47 +1,47 @@
 let weaknessTracking = {
   "MinionLv2": {
-    "Fire": 404,
-    "Thunder": 518,
-    "Quantum": 417,
-    "Wind": 369,
-    "Ice": 463,
-    "Imaginary": 456,
-    "Physical": 436
+    "Fire": 415,
+    "Thunder": 534,
+    "Quantum": 427,
+    "Wind": 376,
+    "Ice": 476,
+    "Imaginary": 474,
+    "Physical": 451
   },
   "Elite": {
-    "Physical": 293,
-    "Ice": 344,
-    "Quantum": 226,
-    "Fire": 331,
-    "Thunder": 335,
-    "Wind": 226,
-    "Imaginary": 299
+    "Physical": 302,
+    "Ice": 347,
+    "Quantum": 229,
+    "Fire": 346,
+    "Thunder": 348,
+    "Wind": 230,
+    "Imaginary": 309
   },
   "LittleBoss": {
-    "Fire": 214,
-    "Thunder": 214,
-    "Quantum": 175,
-    "Imaginary": 216,
-    "Wind": 144,
-    "Physical": 166,
-    "Ice": 153
+    "Fire": 224,
+    "Thunder": 223,
+    "Quantum": 181,
+    "Imaginary": 222,
+    "Wind": 147,
+    "Physical": 169,
+    "Ice": 155
   },
   "BigBoss": {
-    "Fire": 42,
+    "Fire": 50,
     "Thunder": 37,
-    "Quantum": 37,
-    "Wind": 20,
-    "Physical": 31,
-    "Imaginary": 29,
-    "Ice": 26
+    "Quantum": 38,
+    "Wind": 28,
+    "Physical": 32,
+    "Imaginary": 37,
+    "Ice": 27
   },
   "Minion": {
     "Wind": 52,
     "Imaginary": 19,
-    "Physical": 77,
-    "Thunder": 39,
+    "Physical": 83,
+    "Thunder": 41,
     "Quantum": 33,
-    "Ice": 44,
-    "Fire": 56
+    "Ice": 46,
+    "Fire": 58
   }
 }
