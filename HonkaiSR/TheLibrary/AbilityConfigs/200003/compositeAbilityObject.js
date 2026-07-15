c3f6b804bf17fc69b0864bfa9852a1b7b737d826
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-07-14 20:36:56 -0600

    new BE

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/200003/compositeAbilityObject.js
@@ -0,0 +1,10 @@
+const entityPageType = "misc"
+const compositeAbilityObject = {
+  "fullCharacterName": 200003,
+  "trimCharacterName": 200003,
+  "abilityList": [],
+  "abilityObject": {},
+  "enemyData": {},
+  "isBattleEvent": true,
+  "noReader": true
+}
