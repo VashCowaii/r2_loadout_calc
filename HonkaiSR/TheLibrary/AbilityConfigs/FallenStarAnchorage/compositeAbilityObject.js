cb2c20233c4e487971a116f49cc4d4e8974d8a5c
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-07-17 14:58:32 -0600

    new relic pages

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/FallenStarAnchorage/compositeAbilityObject.js
@@ -0,0 +1,16 @@
+const entityPageType = "relic"
+const compositeAbilityObject = {
+  "fullCharacterName": "Fallen Star Anchorage",
+  "trimCharacterName": "FallenStarAnchorage",
+  "abilityList": [],
+  "abilityObject": {},
+  "fixedStats": {
+    "2": {
+      "CritRateBase": 0.08
+    },
+    "4": {}
+  },
+  "isLightcone": true,
+  "isRelic": true,
+  "noReader": true
+}
