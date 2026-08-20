712a4ab9b35da677278767448b1712cbfe57fa47
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-20 07:44:39 -0600

    5012032

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012032/5012032_Monster_W5_JK_Ability01_Part01_configAbility.js
@@ -0,0 +1,31 @@
+const configAbility = {
+  "fileName": "5012032_Monster_W5_JK_Ability01_Part01",
+  "childAbilityList": [
+    "5012032_Monster_W5_JK_Ability01_Camera",
+    "5012032_Monster_W5_JK_Ability01_Part01",
+    "5012032_Monster_W5_JK_Ability01_Part02"
+  ],
+  "skillTrigger": "Skill01",
+  "abilityType": "Skill",
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Trigger Ability",
+      "from": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "ability": "Monster_W5_JK_Ability01_Part02",
+      "isTrigger": true
+    },
+    "Deleted bullshit"
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Ability Target List}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "Select Hostile Target"
+  },
+  "references": []
+}
---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012032/5012032_Monster_W5_JK_AbilityP01_FateRin_configAbility.js
@@ -0,0 +1,21 @@
+const configAbility = {
+  "fileName": "5012032_Monster_W5_JK_AbilityP01_FateRin",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "whenAdded": [
+    {
+      "name": "Random Event",
+      "odds": [
+        1,
+        1,
+        1
+      ]
+    }
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  },
+  "references": []
+}
