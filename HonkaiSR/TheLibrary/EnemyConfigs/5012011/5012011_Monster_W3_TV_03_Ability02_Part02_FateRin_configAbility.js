2c3751f798cc60566f633ac880c38df627838faa
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-20 07:43:38 -0600

    5012011

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012011/5012011_Monster_W3_TV_03_Ability02_Part02_FateRin_configAbility.js
@@ -0,0 +1,65 @@
+const configAbility = {
+  "fileName": "5012011_Monster_W3_TV_03_Ability02_Part02_FateRin",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Add Events/Bonuses",
+      "to": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "modifier": "<a class=\"gModGreen\" id=\"-1742127084\">Enemy_W3_TV_03_Ability02_AttackUp</a>",
+      "valuePerStack": {
+        "MDF_PropertyValue": {
+          "operator": "Variables[0] ({[Skill02[0]]}) || RETURN",
+          "displayLines": "{[Skill02[0]]}",
+          "constants": [],
+          "variables": [
+            "{[Skill02[0]]}"
+          ]
+        }
+      }
+    },
+    "Trigger: Ability End"
+  ],
+  "targetObjectData": {
+    "primaryTarget": "Inherent Target"
+  },
+  "references": [
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-1742127084\">Enemy_W3_TV_03_Ability02_AttackUp</a>",
+      "stackType": "ReplaceByCaster",
+      "stackData": [
+        "MDF_PropertyValue"
+      ],
+      "addStacksPerTrigger": 1,
+      "execute": [
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier",
+          "execute": [
+            {
+              "name": "Stack Target Stat Value",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Modifier Holder}}"
+              },
+              "statName": "&nbsp;<span class=\"descriptionNumberColor\">ATK%</span>&nbsp;",
+              "value": {
+                "operator": "Variables[0] (MDF_PropertyValue) || Variables[1] (_Layer) || MUL || RETURN",
+                "displayLines": "(MDF_PropertyValue * _Layer)",
+                "constants": [],
+                "variables": [
+                  "MDF_PropertyValue",
+                  "_Layer"
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
