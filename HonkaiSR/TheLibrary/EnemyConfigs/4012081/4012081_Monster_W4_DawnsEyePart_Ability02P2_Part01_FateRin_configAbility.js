84d459c67c19a86673ad038c17aa9684118a2703
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-20 04:12:34 -0600

    4012081

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/4012081/4012081_Monster_W4_DawnsEyePart_Ability02P2_Part01_FateRin_configAbility.js
@@ -0,0 +1,31 @@
+const configAbility = {
+  "fileName": "4012081_Monster_W4_DawnsEyePart_Ability02P2_Part01_FateRin",
+  "childAbilityList": [
+    "4012081_Monster_W4_DawnsEyePart_Ability02P2_Camera",
+    "4012081_Monster_W4_DawnsEyePart_Ability02P2_Part01_FateRin",
+    "4012081_Monster_W4_DawnsEyePart_Ability02P2_Part02_FateRin"
+  ],
+  "skillTrigger": "Skill02P2",
+  "abilityType": "Basic ATK",
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Trigger Ability",
+      "from": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "ability": "Monster_W4_DawnsEyePart_Ability02P2_Part02_FateRin",
+      "isTrigger": true
+    },
+    "Deleted bullshit"
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Ability Target List}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "Select Ally Target"
+  },
+  "references": []
+}
