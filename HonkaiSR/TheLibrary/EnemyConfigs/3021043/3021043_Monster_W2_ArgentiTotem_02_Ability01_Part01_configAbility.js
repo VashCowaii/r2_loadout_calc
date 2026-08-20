b8819c0af790be3a559ef3de9cb6d28364647c4c
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-20 03:46:39 -0600

    3021043

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/3021043/3021043_Monster_W2_ArgentiTotem_02_Ability01_Part01_configAbility.js
@@ -0,0 +1,110 @@
+const configAbility = {
+  "fileName": "3021043_Monster_W2_ArgentiTotem_02_Ability01_Part01",
+  "childAbilityList": [
+    "3021043_Monster_W2_ArgentiTotem_02_Ability01_Part01",
+    "3021043_Monster_W2_ArgentiTotem_02_Ability01_Part02",
+    "3021043_Monster_W2_ArgentiTotem_02_Ability01_Camera",
+    "3021043_Monster_W2_ArgentiTotem_02_Ability02_Camera",
+    "3021043_Monster_W2_ArgentiTotem_02_AbilityP01_Insert_FateRin",
+    "3021043_Monster_W2_ArgentiTotem_02_AbilityP01_Part01_FateRin",
+    "3021043_Monster_W2_ArgentiTotem_02_AbilityP01_Camera",
+    "3021043_Monster_W2_ArgentiTotem_02_AbilityP02_Camera"
+  ],
+  "skillTrigger": "Skill01",
+  "abilityType": "Basic ATK",
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Exit Broken-State",
+      "target": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      }
+    },
+    {
+      "name": "Trigger Ability",
+      "from": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "ability": "Monster_W2_ArgentiTotem_02_Ability01_Part02",
+      "isTrigger": true
+    },
+    {
+      "name": "IF",
+      "conditions": {
+        "name": "OR",
+        "conditionList": [
+          {
+            "name": "Has Modifier",
+            "target": {
+              "name": "Target Name",
+              "target": "{{Caster}}"
+            },
+            "modifier": "<a class=\"gModGreen\" id=\"-431168288\">Enemy_W2_Argenti_Power</a>[<span class=\"descriptionNumberColor\">Soulfreed</span>]"
+          },
+          {
+            "name": "Has Modifier",
+            "target": {
+              "name": "Target Name",
+              "target": "{{Caster}}"
+            },
+            "modifier": "<a class=\"gModGreen\" id=\"-488812813\">Enemy_W2_Argenti_Power_RL</a>[<span class=\"descriptionNumberColor\">Soul Ascension</span>]"
+          }
+        ]
+      },
+      "passed": [
+        {
+          "name": "Find New Target",
+          "from": {
+            "name": "Target Name",
+            "target": "{{All Team Members(Exclude Self)}}"
+          },
+          "searchRandom": true,
+          "maxTargets": 1,
+          "conditions": {
+            "name": "Check Boolean Value",
+            "target": {
+              "name": "Target Name",
+              "target": "{{Parameter Target}}"
+            },
+            "value": "PowerFlag"
+          },
+          "ifTargetFound": [
+            "Deleted bullshit"
+          ]
+        }
+      ],
+      "failed": [
+        {
+          "name": "Find New Target",
+          "from": {
+            "name": "Target Name",
+            "target": "{{All Team Members(Exclude Self)}}"
+          },
+          "searchRandom": true,
+          "maxTargets": 1,
+          "conditions": {
+            "name": "Check Boolean Value",
+            "target": {
+              "name": "Target Name",
+              "target": "{{Parameter Target}}"
+            },
+            "value": "PowerFlag"
+          },
+          "ifTargetFound": [
+            "Deleted bullshit"
+          ]
+        }
+      ]
+    }
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Ability Target List}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "Select Ally Target"
+  },
+  "references": []
+}
---
+++ HonkaiSR/TheLibrary/EnemyConfigs/3021043/3021043_Monster_W2_ArgentiTotem_02_PassiveAbilityInitiate_FateRin_configAbility.js
@@ -0,0 +1,277 @@
+const configAbility = {
+  "fileName": "3021043_Monster_W2_ArgentiTotem_02_PassiveAbilityInitiate_FateRin",
+  "skillTrigger": "SkillP01",
+  "abilityType": "Basic ATK",
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Add Events/Bonuses",
+      "to": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "modifier": "<a class=\"gModGreen\" id=\"1166907060\">Enemy_Standard_MuteHitFly</a>"
+    },
+    {
+      "name": "Add Events/Bonuses",
+      "to": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "modifier": "<a class=\"gModGreen\" id=\"-899723889\">Monster_ArgentiTotem_ListenToSummer</a>"
+    },
+    {
+      "name": "Add Events/Bonuses",
+      "to": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "modifier": "<a class=\"gModGreen\" id=\"-1041296063\">Enemy_W2_ArgentiTotem_02_Initiate_FateRin</a>"
+    }
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "{{Caster}}"
+  },
+  "references": [
+    {
+      "name": "Modifier Construction",
+      "for": "<a class=\"gModGreen\" id=\"mod__-1041296063\">Enemy_W2_ArgentiTotem_02_Initiate_FateRin</a>",
+      "modifierFlags": [
+        "Deathrattle",
+        "KeepOnDeathrattle"
+      ],
+      "execute": [
+        {
+          "eventTrigger": "Turn [Anyone]: End",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "OR",
+                "conditionList": [
+                  {
+                    "name": "Has Modifier",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Modifier Holder}}"
+                    },
+                    "modifier": "<a class=\"gModGreen\" id=\"-431168288\">Enemy_W2_Argenti_Power</a>[<span class=\"descriptionNumberColor\">Soulfreed</span>]"
+                  },
+                  {
+                    "name": "Has Modifier",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Modifier Holder}}"
+                    },
+                    "modifier": "<a class=\"gModGreen\" id=\"-488812813\">Enemy_W2_Argenti_Power_RL</a>[<span class=\"descriptionNumberColor\">Soul Ascension</span>]"
+                  }
+                ],
+                "invertCondition": true
+              }
+            }
+          ]
+        },
+        {
+          "eventTrigger": "Being Limbo [Owner]",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "OR",
+                "conditionList": [
+                  {
+                    "name": "Has Modifier",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Caster}}"
+                    },
+                    "modifier": "<a class=\"gModGreen\" id=\"-431168288\">Enemy_W2_Argenti_Power</a>[<span class=\"descriptionNumberColor\">Soulfreed</span>]"
+                  },
+                  {
+                    "name": "Has Modifier",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Caster}}"
+                    },
+                    "modifier": "<a class=\"gModGreen\" id=\"-488812813\">Enemy_W2_Argenti_Power_RL</a>[<span class=\"descriptionNumberColor\">Soul Ascension</span>]"
+                  }
+                ]
+              },
+              "passed": [
+                {
+                  "name": "Declare Custom Variable",
+                  "target": {
+                    "name": "Target Name",
+                    "target": "{{Caster}}"
+                  },
+                  "scope": "TargetEntity",
+                  "variableName": "PowerFlag",
+                  "value": 1
+                }
+              ]
+            },
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "AND",
+                "conditionList": [
+                  {
+                    "name": "Is Part Of Team",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "team": "Player Team",
+                    "invertCondition": true
+                  },
+                  {
+                    "name": "Compare: Variable",
+                    "value1": "PowerFlag",
+                    "compareType": "=",
+                    "value2": 0,
+                    "contextScope": "TargetEntity"
+                  }
+                ]
+              },
+              "passed": [
+                "Modifier Deletes Itself"
+              ]
+            }
+          ]
+        },
+        {
+          "eventTrigger": "When Stacking/Receiving Modifier"
+        },
+        {
+          "eventTrigger": "Was Killed (Queued) [Owner]",
+          "execute": [
+            {
+              "name": "Dispel Debuffs",
+              "target": {
+                "name": "Target Name",
+                "target": "{{Caster}}"
+              },
+              "silent": true
+            },
+            {
+              "name": "Mark Entity For Immediate Death"
+            },
+            {
+              "name": "Inject Ability Use",
+              "checkOverride": {
+                "name": "Condition Priority",
+                "overridePriority": "MonsterForceKill",
+                "condition": {
+                  "name": "Compare: Ability Value",
+                  "target": {
+                    "name": "Target Sequence",
+                    "Sequence": [
+                      {
+                        "name": "Target Name",
+                        "target": "{{All Team Members}}"
+                      },
+                      {
+                        "name": "Target Filter",
+                        "conditions": {
+                          "name": "Check Boolean Value",
+                          "target": {
+                            "name": "Target Name",
+                            "target": "{{Parameter Target}}"
+                          },
+                          "value": "PowerFlag"
+                        }
+                      }
+                    ]
+                  },
+                  "value1": "&nbsp;<span class=\"descriptionNumberColor\">HPCurrent</span>&nbsp;",
+                  "compareType": "<=",
+                  "value2": 0
+                }
+              },
+              "abilityName": "Monster_W2_ArgentiTotem_02_AbilityP01_Insert_FateRin",
+              "abilitySource": {
+                "name": "Target Name",
+                "target": "{{Caster}}"
+              },
+              "abilityTarget": {
+                "name": "Add Target by Unique Identifier",
+                "identifier": "Activity_FateRin_MasterAvatar"
+              },
+              "priorityTag": "EnemyDeathEffect",
+              "ownerState": "Mask_AliveOrLimbo",
+              "targetState": "Mask_AliveOrLimbo",
+              "canHitNonTargets": true,
+              "showInActionOrder": true,
+              "abortFlags": [
+                "STAT_MonsterChangePhase"
+              ],
+              "allowAbilityTriggers": false
+            }
+          ]
+        },
+        {
+          "eventTrigger": "When Modifier is Added [Owner]",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "OR",
+                "conditionList": [
+                  {
+                    "name": "Modifier Was",
+                    "modifier": "<a class=\"gModGreen\" id=\"-431168288\">Enemy_W2_Argenti_Power</a>[<span class=\"descriptionNumberColor\">Soulfreed</span>]"
+                  },
+                  {
+                    "name": "Modifier Was",
+                    "modifier": "<a class=\"gModGreen\" id=\"-488812813\">Enemy_W2_Argenti_Power_RL</a>[<span class=\"descriptionNumberColor\">Soul Ascension</span>]"
+                  }
+                ]
+              }
+            }
+          ]
+        },
+        {
+          "eventTrigger": "Modifier Cleansed [Anyone]",
+          "execute": [
+            {
+              "name": "IF",
+              "conditions": {
+                "name": "AND",
+                "conditionList": [
+                  {
+                    "name": "Compare: Target",
+                    "target": {
+                      "name": "Target Name",
+                      "target": "{{Parameter Target}}"
+                    },
+                    "target2": {
+                      "name": "Target Name",
+                      "target": "{{Modifier Holder}}"
+                    }
+                  },
+                  {
+                    "name": "OR",
+                    "conditionList": [
+                      {
+                        "name": "Modifier Was",
+                        "modifier": "<a class=\"gModGreen\" id=\"-431168288\">Enemy_W2_Argenti_Power</a>[<span class=\"descriptionNumberColor\">Soulfreed</span>]"
+                      },
+                      {
+                        "name": "Modifier Was",
+                        "modifier": "<a class=\"gModGreen\" id=\"-488812813\">Enemy_W2_Argenti_Power_RL</a>[<span class=\"descriptionNumberColor\">Soul Ascension</span>]"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
