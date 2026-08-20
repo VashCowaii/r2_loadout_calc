e3f22acfb5376f195c52ea12cb73cc5386f93ae8
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-20 07:34:30 -0600

    4062021

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/4062021/4062021_Monster_W2_Beast01_04_Ability01_Part01_configAbility.js
@@ -0,0 +1,31 @@
+const configAbility = {
+  "fileName": "4062021_Monster_W2_Beast01_04_Ability01_Part01",
+  "childAbilityList": [
+    "4062021_Monster_W2_Beast01_04_Ability01_Camera",
+    "4062021_Monster_W2_Beast01_04_Ability01_Part01",
+    "4062021_Monster_W2_Beast01_04_Ability01_Part02"
+  ],
+  "skillTrigger": "Skill01",
+  "abilityType": "Skill",
+  "energy": null,
+  "toughnessList": null,
+  "parse": [
+    {
+      "name": "Trigger Ability",
+      "from": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "ability": "Monster_W2_Beast01_04_Ability01_Part02",
+      "isTrigger": true
+    },
+    "Deleted bullshit"
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Ability Target List}}"
+  },
+  "realTargetData": {
+    "primaryTarget": "Select Hostile Target"
+  },
+  "references": []
+}
