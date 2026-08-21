4f502cc6fcc22c76f64bbb79f08809b5a28c88c8
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-21 05:26:54 -0600

    5012101

---
+++ HonkaiSR/TheLibrary/EnemyConfigs/5012101/5012101_Monster_W5_Magical_Passive01_FateRin_configAbility.js
@@ -0,0 +1,21 @@
+const configAbility = {
+  "fileName": "5012101_Monster_W5_Magical_Passive01_FateRin",
+  "abilityType": null,
+  "energy": null,
+  "toughnessList": null,
+  "parse": [],
+  "whenAdded": [
+    {
+      "name": "Change Entity Turn-State",
+      "target": {
+        "name": "Target Name",
+        "target": "{{Caster}}"
+      },
+      "phase": "Default"
+    }
+  ],
+  "targetObjectData": {
+    "primaryTarget": "{{Caster}}"
+  },
+  "references": []
+}
