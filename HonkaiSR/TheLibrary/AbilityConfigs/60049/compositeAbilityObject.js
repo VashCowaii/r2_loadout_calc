141f2d454cc3fb0a93649b0bf87cad76f28ff279
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-26 05:22:21 -0600

    new BE pages

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/60049/compositeAbilityObject.js
@@ -0,0 +1,67 @@
+const entityPageType = "misc"
+const compositeAbilityObject = {
+  "fullCharacterName": 60049,
+  "trimCharacterName": 60049,
+  "abilityList": [],
+  "abilityObject": {},
+  "enemyData": {
+    "Skill03": {
+      "skillID": 6004901,
+      "trigger": "Skill03",
+      "name": "Epics, as We've Written",
+      "type": "AoE",
+      "slot": "Ultimate",
+      "desc": "Ally targets using Ultimate can regenerate 1 Energy, deal massive True DMG to all enemies, and greatly reduce \"Amphorean Hatred\"!",
+      "energyCost": 4,
+      "energyRegen": null,
+      "energyRate": 0.5,
+      "toughnessReductionDisplayed": null,
+      "skillPointCost": 0,
+      "skillPointGain": 0,
+      "params": {
+        "1": [
+          0.012,
+          0.2,
+          0.06
+        ]
+      },
+      "skillEffect": "AoEAttack",
+      "maxLevel": 1,
+      "configAbilityList": null,
+      "toughnessList": [
+        0,
+        120,
+        0
+      ],
+      "hitSplits": []
+    },
+    "SkillP01": {
+      "skillID": 6004902,
+      "trigger": "SkillP01",
+      "name": null,
+      "type": "Talent",
+      "slot": "Talent",
+      "desc": "",
+      "energyCost": null,
+      "energyRegen": null,
+      "energyRate": 0.5,
+      "toughnessReductionDisplayed": null,
+      "skillPointCost": 0,
+      "skillPointGain": 0,
+      "params": {
+        "1": []
+      },
+      "skillEffect": "Passive",
+      "maxLevel": 1,
+      "configAbilityList": null,
+      "toughnessList": [
+        0,
+        0,
+        0
+      ],
+      "hitSplits": []
+    }
+  },
+  "isBattleEvent": true,
+  "noReader": true
+}
