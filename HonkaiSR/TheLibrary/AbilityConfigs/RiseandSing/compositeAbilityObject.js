9dee5ff8421f9d2821d9e95f9d933925cc016271
VashCowaii <160493873+VashCowaii@users.noreply.github.com>
2026-08-27 08:37:15 -0600

    new lc pages

---
+++ HonkaiSR/TheLibrary/AbilityConfigs/RiseandSing/compositeAbilityObject.js
@@ -0,0 +1,26 @@
+const entityPageType = "lc"
+const compositeAbilityObject = {
+  "fullCharacterName": "Rise and Sing",
+  "trimCharacterName": "RiseandSing",
+  "abilityList": [],
+  "fixedStats": {
+    "1": {
+      "HP%": 0.3
+    },
+    "2": {
+      "HP%": 0.375
+    },
+    "3": {
+      "HP%": 0.45
+    },
+    "4": {
+      "HP%": 0.525
+    },
+    "5": {
+      "HP%": 0.6
+    }
+  },
+  "abilityObject": {},
+  "noReader": true,
+  "isLightcone": true
+}
